d89ca6e20cc0e715cc63745c3302f227dcf51076
Jorge <jorge.softdevelop@gmail.com>
2017-05-15 12:57:48 -0400

    Prueba 1 de uso de junit con JaCoCo en integracion con Jenkins y maven

---
+++ soaint-enterprise-services/correspondencia-business-services/src/main/java/co/com/soaint/correspondencia/Palindrome.java
@@ -0,0 +1,17 @@
+package co.com.soaint.correspondencia;
+
+/**
+ * Created by Jorge on 15/05/2017.
+ */
+public class Palindrome {
+    public boolean isPalindrome(String inputString) {
+        if (inputString.length() == 0) {
+            return true;
+        } else {
+            char firstChar = inputString.charAt(0);
+            char lastChar = inputString.charAt(inputString.length() - 1);
+            String mid = inputString.substring(1, inputString.length() - 1);
+            return (firstChar == lastChar) && isPalindrome(mid);
+        }
+    }
+}
