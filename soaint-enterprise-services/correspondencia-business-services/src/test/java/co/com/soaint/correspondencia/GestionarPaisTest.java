2a62b6aef6fed133abb112dc1d21c03478f45ad6
Jorge <jorge.softdevelop@gmail.com>
2017-05-15 14:45:13 -0400

    Ejemplo de prueba unitaria con BD interna.

---
+++ soaint-enterprise-services/correspondencia-business-services/src/test/java/co/com/soaint/correspondencia/GestionarPaisTest.java
@@ -0,0 +1,23 @@
+package co.com.soaint.correspondencia;
+
+import co.com.soaint.correspondencia.domain.entity.TvsPais;
+import co.com.soaint.foundation.canonical.correspondencia.PaisDTO;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Jorge on 15/05/2017.
+ */
+public class GestionarPaisTest extends  JPAHibernateTest {
+
+    @Test
+    public void testTvsPais_findAll_success() {
+        String estado = "ACTIVO";
+        List<PaisDTO> paises = em.createNamedQuery("TvsPais.findAll", PaisDTO.class).setParameter("ESTADO", estado).getResultList();
+        assertEquals(1, paises.size());
+        assertEquals("CUBA",paises.get(0).getNombre());
+    }
+}
