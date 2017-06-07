4166254d8c0bf47d13c7dceccef0ca48068c3006
Jorge <jorge.softdevelop@gmail.com>
2017-06-07 14:33:33 -0400

    Prueba de unidad para GestionarDepartamentos

--- soaint-sgd-enterprise-services/correspondencia-business-services/src/test/java/co/com/soaint/correspondencia/GestionarDepartamentoTest.java
+++ soaint-sgd-enterprise-services/correspondencia-business-services/src/test/java/co/com/soaint/correspondencia/GestionarDepartamentoTest.java
@@ -1,51 +1,26 @@
 package co.com.soaint.correspondencia;
 
-import co.com.soaint.foundation.canonical.correspondencia.MunicipioDTO;
+import co.com.soaint.foundation.canonical.correspondencia.DepartamentoDTO;
+import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hamcrest.collection.IsEmptyCollection;
-
-
 import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-
 
 /**
  * Created by Jorge on 07/06/2017.
  */
-public class GestionarMunicipioTest extends  JPAHibernateTest{
-
-
-    @Test
-    public void listarMunicipiosByCodDeparAndEstado_success() {
-        String estado = "ACTIVO";
-        String codDepar = "CODIGODEPTO1";
-        List<MunicipioDTO> listado = em.createNamedQuery("TvsMunicipio.findAllByCodDeparAndEstado", MunicipioDTO.class)
-                .setParameter("COD_DEPAR", codDepar)
-                .setParameter("ESTADO", estado)
-                .getResultList();
-        Assert.assertNotNull("La lista no debe ser nula", listado);
-
-        assertThat(listado, not(IsEmptyCollection.empty()));
-        assertThat(new ArrayList<>(), IsEmptyCollection.empty());
-        assertThat(listado, hasSize(1));
-
-    }
-
+public class GestionarDepartamentoTest extends  JPAHibernateTest {
 
     @Test
-    public void listarMunicipiosByEstado_success() {
+    public void listarDepartamentosByEstado_success() {
         String estado = "ACTIVO";
-
-        List<MunicipioDTO> listado = em.createNamedQuery("TvsMunicipio.findAll", MunicipioDTO.class)
+        List<DepartamentoDTO> listado = em.createNamedQuery("TvsDepartamento.findAll", DepartamentoDTO.class)
                 .setParameter("ESTADO", estado)
                 .getResultList();
         Assert.assertNotNull("La lista no debe ser nula", listado);
@@ -53,6 +28,22 @@ public class GestionarMunicipioTest extends  JPAHibernateTest{
         assertThat(listado, not(IsEmptyCollection.empty()));
         assertThat(new ArrayList<>(), IsEmptyCollection.empty());
         assertThat(listado, hasSize(2));
+
+    }
+
+    @Test
+    public void listarMunicipiosByCodDeparAndEstado_success() {
+        String estado = "ACTIVO";
+        String codPais = "CODIGOPAIS1";
+        List<DepartamentoDTO> listado = em.createNamedQuery("TvsDepartamento.findAllByCodPaisAndEstado", DepartamentoDTO.class)
+                .setParameter("COD_PAIS", codPais)
+                .setParameter("ESTADO", estado)
+                .getResultList();
+        Assert.assertNotNull("La lista no debe ser nula", listado);
+
+        assertThat(listado, not(IsEmptyCollection.empty()));
+        assertThat(new ArrayList<>(), IsEmptyCollection.empty());
+        assertThat(listado, hasSize(1));
 
 
     }
