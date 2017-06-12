cbe47894907257cd070015949a3c7906bc5073ca
Dasiel Otero Dartayet <dasiel@gmail.com>
2017-06-12 13:58:25 -0400

    MOD Se movieron los metodos alfresco desde la clase utilities para la clase  ContentControlAlfresco

--- soaint-sgd-enterprise-services/ecm-integration-services/src/main/java/co/com/soaint/ecm/business/boundary/documentmanager/ContentControlAlfresco.java
+++ soaint-sgd-enterprise-services/ecm-integration-services/src/main/java/co/com/soaint/ecm/business/boundary/documentmanager/ContentControlAlfresco.java
@@ -13,13 +13,13 @@ import org.apache.chemistry.opencmis.client.api.*;
 import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl;
 import org.apache.chemistry.opencmis.commons.PropertyIds;
 import org.apache.chemistry.opencmis.commons.SessionParameter;
+import org.apache.chemistry.opencmis.commons.enums.BaseTypeId;
 import org.apache.chemistry.opencmis.commons.enums.BindingType;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisObjectNotFoundException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.logging.Logger;
 
@@ -262,6 +262,140 @@ public class ContentControlAlfresco extends ContentControl {
             LOGGER.info ("*** Error al crear folder ***");
         }
         return newFolder;
+    }
+
+    /**
+     * Método que, dado el nombre de un espacio, devuelve su Id (nodeRef)
+     * @param session
+     * @param nombre
+     * @return nodeRef (id)
+     */
+    public String obtenerIdDocumento(Session session, String nombre){
+
+        String queryString = "SELECT cmis:objectId FROM cmis:document WHERE cmis:name = '" + nombre+"'";
+
+        ItemIterable<QueryResult> results = session.query(queryString, false);
+
+        String id = "";
+
+        for (QueryResult qResult : results) {
+            String objectId = qResult.getPropertyValueByQueryName("cmis:objectId");
+            Document document = (Document) session.getObject(session.createObjectId(objectId));
+            id = document.getId();
+            System.out.println("Listado de carpetas: "+document.getId());
+        }
+        return id;
+
+    }
+
+    /**
+     * Método que, dado la sesión y el nombre del documento, devuelve su contenido (InputStream)
+     * @param session
+     * @param nombre
+     * @return InputStream(stream)
+     */
+    public InputStream obtenerContenidoDocumento(Session session, String nombre){
+        CmisObject object = session.getObject(session.createObjectId(obtenerIdDocumento (session,nombre)));
+        Document document;
+        InputStream stream=null;
+        if (object instanceof Document){
+            document = (Document) object;
+            String filename = document.getName();
+            stream = document.getContentStream().getStream();
+        }
+        else{
+            System.out.println("No es de tipo document");
+        }
+        return stream;
+    }
+
+    /**
+     * Método que, dado el nombre de una Carpeta, devuelve la carpeta(carpeta)
+     * @param session
+     * @param nombre
+     * @return Folder (carpeta)
+     */
+    public static Folder obtenerCarpetaPorNombre(Session session, String nombre)    {
+
+
+
+        String queryString = "SELECT cmis:objectId FROM cmis:folder WHERE cmis:name = '" + nombre+"'";
+
+        ItemIterable<QueryResult> results = session.query(queryString, false);
+
+        Folder carpeta = null;
+
+
+
+        for (QueryResult qResult : results) {
+            String objectId = qResult.getPropertyValueByQueryName("cmis:objectId");
+            carpeta = (Folder) session.getObject(session.createObjectId(objectId));
+
+            System.out.println("Listado de carpetas: "+carpeta.getId());
+        }
+
+
+        return carpeta;
+
+    }
+
+    /**
+     * Método que, dado una Carpeta y la profundidad necesaria, devuelve una lista de las carpetas (carpeta)
+     * @param depth
+     * @param target
+     * @return
+     */
+    private static void obtenerlistaCarpetas(int depth, Folder target) {
+        String indent = StringUtils.repeat("\t", depth);
+        for (Iterator<CmisObject> it = target.getChildren().iterator(); it.hasNext();) {
+            CmisObject o = it.next();
+            if (BaseTypeId.CMIS_DOCUMENT.equals(o.getBaseTypeId())) {
+                System.out.println(indent + "[Docment] " + o.getName());
+            } else if (BaseTypeId.CMIS_FOLDER.equals(o.getBaseTypeId())) {
+                System.out.println(indent + "[Folder] " + o.getName());
+                obtenerlistaCarpetas(++depth, (Folder) o);
+            }
+        }
+
+    }
+
+    /**
+     * Método que, dado el nombre de una Carpeta y un documento, elimina el docuemtno(carpeta)
+     * @param target
+     * @param delDocName
+     * @param session
+     */
+    private void eliminarDocumento(Folder target, String delDocName,Session session) {
+
+
+        try {
+
+            CmisObject object = session.getObjectByPath(target.getPath()+ delDocName);
+            Document delDoc = (Document) object;
+            delDoc.delete(true);
+
+        } catch (CmisObjectNotFoundException e) {
+            System.err.println("Document is not found: " + delDocName);
+
+        }
+    }
+
+
+    /**
+     * Metodo para mover un documento de una carpeta a otra
+     * @param targetFolder
+     * @param sourceFolder
+     * @param document
+     */
+    public void moverDocumento(String document, Folder sourceFolder, Folder targetFolder, Session session) {
+        try {
+            CmisObject object = session.getObjectByPath(sourceFolder.getPath()+"/"+document);
+            Document mvndocument = (Document) object;
+            mvndocument.move(sourceFolder,targetFolder);
+        } catch (CmisObjectNotFoundException e) {
+            System.err.println("Document is not found: " + document);
+        }
+
     }
 
     public String  formatearNombre(String[] informationArray, String formatoConfig) throws SystemException {
