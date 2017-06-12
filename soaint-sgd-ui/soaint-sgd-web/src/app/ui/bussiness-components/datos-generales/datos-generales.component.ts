7a23e73e64836ef04487cb1f5d4f2f49ad271ab1
Daniel Barrios <dbarrios1907@gmail.com>
2017-06-13 00:31:37 +0200

    Reestructurando componentes de radicaci'on de documentos, mejorando formularios

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/bussiness-components/datos-generales/datos-generales.component.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/bussiness-components/datos-generales/datos-generales.component.ts
@@ -1,119 +1,118 @@
-// import {Component, OnInit} from '@angular/core';
-// import {DatosGeneralesModel} from './datos-generales.model';
-// import {ListaSeleccionSimple} from '../../../domain/lista.seleccion.simple';
-// import {
-//   MediosRecepcionApiService,
-//   TipoComunicacionApiService,
-//   TipologiaDocumentalApiService,
-//   UnidadTiempoApiService
-// } from 'app/infrastructure/__api.include';
-//
-// @Component({
-//   selector: 'app-datos-generales',
-//   templateUrl: './datos-generales.component.html'
-// })
-// export class DatosGeneralesComponent implements OnInit {
-//
-//   tipoComunicacionSeleccionado: ListaSeleccionSimple;
-//   medioRecepcionSeleccionado: ListaSeleccionSimple;
-//   tipologiaDocumentalSeleccionada: ListaSeleccionSimple;
-//   unidadTiempoSeleccionada: ListaSeleccionSimple;
-//
-//   model: DatosGeneralesModel;
-//
-//   tiposComunicacionFiltrados: Array<ListaSeleccionSimple>;
-//
-//   mediosRecepcionFiltrados: Array<ListaSeleccionSimple>;
-//
-//   tipologiasDocumentalesFiltradas: Array<ListaSeleccionSimple>;
-//
-//   unidadesTiempoFiltradas: Array<ListaSeleccionSimple>;
-//
-//   checkboxValues: string[] = [];
-//
-//   constructor(private _tipoComunicacionApi: TipoComunicacionApiService, private _medioRecepcionApi: MediosRecepcionApiService,
-//               private _tipologiaDocumentalApi: TipologiaDocumentalApiService, private _unidadTiempoApi: UnidadTiempoApiService) {
-//   }
-//
-//
-//   ngOnInit(): void {
-//   }
-//
-//   filtrarTiposComunicacion(event) {
-//     this.tiposComunicacionFiltrados = [];
-//     for (let i = 0; i < this.model.tiposComunicacion.length; i++) {
-//       const brand = this.model.tiposComunicacion[i];
-//       if (brand.nombre.toLowerCase().indexOf(event.query.toLowerCase()) == 0) {
-//         this.tiposComunicacionFiltrados.push(brand);
-//       }
-//     }
-//   }
-//
-//   filtrarMeidosRecepcion(event) {
-//     this.mediosRecepcionFiltrados = [];
-//     for (let i = 0; i < this.model.mediosRecepcion.length; i++) {
-//       const medioRecepcion = this.model.mediosRecepcion[i];
-//       if (medioRecepcion.nombre.toLowerCase().indexOf(event.query.toLowerCase()) == 0) {
-//         this.mediosRecepcionFiltrados.push(medioRecepcion);
-//       }
-//     }
-//   }
-//
-//   filtrarTipologiasDocumentales(event) {
-//     this.tipologiasDocumentalesFiltradas = [];
-//     for (let i = 0; i < this.model.tipologiasDocumentales.length; i++) {
-//       const tipologiaDocumental = this.model.tipologiasDocumentales[i];
-//       if (tipologiaDocumental.nombre.toLowerCase().indexOf(event.query.toLowerCase()) == 0) {
-//         this.tipologiasDocumentalesFiltradas.push(tipologiaDocumental);
-//       }
-//     }
-//   }
-//
-//   filtrarUnidadesTiempo(event) {
-//     this.unidadesTiempoFiltradas = [];
-//     for (let i = 0; i < this.model.unidadesTiempo.length; i++) {
-//       const unidadTiempo = this.model.unidadesTiempo[i];
-//       if (unidadTiempo.nombre.toLowerCase().indexOf(event.query.toLowerCase()) == 0) {
-//         this.unidadesTiempoFiltradas.push(unidadTiempo);
-//       }
-//     }
-//   }
-//
-//   onTiposComunicacionDropDownClick() {
-//     this.tiposComunicacionFiltrados = [];
-//
-//     this._tipoComunicacionApi.list().subscribe(
-//       data => this.tiposComunicacionFiltrados = data,
-//       error => console.log('Error searching in products api ' + error)
-//     );
-//   }
-//
-//   onMediosRecepcionDropDownClick() {
-//     this.mediosRecepcionFiltrados = [];
-//
-//     this._medioRecepcionApi.list().subscribe(
-//       data => this.mediosRecepcionFiltrados = data,
-//       error => console.log('Error searching in products api ' + error)
-//     );
-//   }
-//
-//   onTipologiasDocumentalesDropDownClick() {
-//     this.tipologiasDocumentalesFiltradas = [];
-//
-//     this._tipologiaDocumentalApi.list().subscribe(
-//       data => this.tipologiasDocumentalesFiltradas = data,
-//       error => console.log('Error searching in tipologiasDocumentales api ' + error)
-//     );
-//   }
-//
-//   onUnidadesTiempoDropDownClick() {
-//     this.unidadesTiempoFiltradas = [];
-//
-//     this._unidadTiempoApi.list().subscribe(
-//       data => this.unidadesTiempoFiltradas = data,
-//       error => console.log('Error searching in tipologiasDocumentales api ' + error)
-//     );
-//   }
-//
-//
-// }
+import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {ConstanteDTO} from 'app/domain/constanteDTO';
+import {Store} from '@ngrx/store';
+import {State} from 'app/infrastructure/redux-store/redux-reducers';
+import { Sandbox } from 'app/infrastructure/state-management/constanteDTO-state/constanteDTO-sandbox';
+import { getTipoComunicacionArrayData } from 'app/infrastructure/state-management/constanteDTO-state/constanteDTO-selectors';
+
+@Component({
+  selector: 'app-datos-generales',
+  templateUrl: './datos-generales.component.html',
+  styles: [`
+    .ui-datalist-header, .ui-datatable-header {
+      text-align: left !important;
+    }
+  `],
+  encapsulation: ViewEncapsulation.None
+})
+export class DatosGeneralesComponent implements OnInit {
+
+
+  tipoComunicacionSuggestions$: Observable<ConstanteDTO[]>;
+
+
+  constructor(private _store: Store<State>, private _sandbox: Sandbox) {
+  }
+
+  ngOnInit(): void {
+    this.tipoComunicacionSuggestions$ = this._store.select(getTipoComunicacionArrayData);
+  }
+
+  onSelectTipoComunicacion(value) {
+    console.info(value);
+  }
+
+  onFilterTipoComunicacion($event) {
+    const query = $event.query;
+    this._sandbox.filterDispatch('tipoComunicacion' , query);
+  }
+
+  onDropdownClickTipoComunicacion($event) {
+    // this method triggers load of suggestions
+    this._sandbox.loadDispatch('tipoComunicacion');
+  }
+
+  onFilterTipoPersona($event) {
+    const query = $event.query;
+    this._sandbox.filterDispatch('tipoPersona' , query);
+  }
+
+  onDropdownClickTipoPersona($event) {
+    // this method triggers load of suggestions
+    this._sandbox.loadDispatch('tipoPersona');
+  }
+
+  onFilterTipoAnexos($event) {
+    const query = $event.query;
+    this._sandbox.filterDispatch('tipoAnexos' , query);
+  }
+
+  onDropdownClickTipoAnexos($event) {
+    // this method triggers load of suggestions
+    this._sandbox.loadDispatch('tipoAnexos');
+  }
+
+  onFilterTipoDocumento($event) {
+    const query = $event.query;
+    this._sandbox.filterDispatch('tipoDocumento' , query);
+  }
+
+  onDropdownClickTipoDocumento($event) {
+    // this method triggers load of suggestions
+    this._sandbox.loadDispatch('tipoDocumento');
+  }
+
+  onFilterTipoTelefono($event) {
+    const query = $event.query;
+    this._sandbox.filterDispatch('tipoTelefono' , query);
+  }
+
+  onDropdownClickTipoTelefono($event) {
+    // this method triggers load of suggestions
+    this._sandbox.loadDispatch('tipoTelefono');
+  }
+
+  onFilterTipoDestinatario($event) {
+    const query = $event.query;
+    this._sandbox.filterDispatch('tipoDestinatario' , query);
+  }
+
+  onDropdownClickTipoDestinatario($event) {
+    // this method triggers load of suggestions
+    this._sandbox.loadDispatch('tipoDestinatario');
+  }
+
+  onFilterUnidadTiempo($event) {
+    const query = $event.query;
+    this._sandbox.filterDispatch('unidadTiempo' , query);
+  }
+
+  onDropdownClickUnidadTiempo($event) {
+    // this method triggers load of suggestions
+    this._sandbox.loadDispatch('unidadTiempo');
+  }
+
+  onFilterMediosRecepcion($event) {
+    const query = $event.query;
+    this._sandbox.filterDispatch('mediosRecepcion' , query);
+  }
+
+  onDropdownClickMediosRecepcion($event) {
+    // this method triggers load of suggestions
+    this._sandbox.loadDispatch('mediosRecepcion');
+  }
+
+
+
+}
