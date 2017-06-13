a72c28e68bb3a204d7dc6fbb3cafdee133ce4a88
leinier <leinieralvarez@gmail.com>
2017-06-13 16:03:39 -0400

    TratamientoCortesiaGatewayApi

---
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/delegator/TratamientoCortesiaClient.java
@@ -0,0 +1,28 @@
+package co.com.foundation.sgd.apigateway.apis.delegator;
+
+import co.com.foundation.sgd.infrastructure.ApiDelegator;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+@ApiDelegator
+public class TratamientoCortesiaClient {
+
+    @Value("${backapi.endpoint.url}")
+    private String endpoint = "";
+
+    public TratamientoCortesiaClient() {
+        super();
+    }
+
+    public Response list() {
+        System.out.println("Tipo Teléfono - [trafic] - listing Tratamiento Cortesia with endpoint: " + endpoint);
+        WebTarget wt = ClientBuilder.newClient().target(endpoint);
+        return wt.path("/constantes-web-api/constantes/hijos/TRAT-COR/A")
+                .request()
+                .get();
+    }
+
+}
