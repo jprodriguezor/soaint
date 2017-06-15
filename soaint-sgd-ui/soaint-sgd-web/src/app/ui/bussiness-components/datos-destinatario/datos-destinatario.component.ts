d35c406e40ef1356dd28cbe23f99e85ac3b4bb5f
leinier <leinieralvarez@gmail.com>
2017-06-15 13:04:45 -0400

    Validaciones de campos requeridos en datos remitente y datos destinatario de radicar comunicaciones

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/bussiness-components/datos-destinatario/datos-destinatario.component.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/bussiness-components/datos-destinatario/datos-destinatario.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/operator/filter';
 import {ConstanteDTO} from 'app/domain/constanteDTO';
@@ -6,10 +6,11 @@ import {Store} from '@ngrx/store';
 import {State} from 'app/infrastructure/redux-store/redux-reducers';
 import {Sandbox} from 'app/infrastructure/state-management/constanteDTO-state/constanteDTO-sandbox';
 import {
-  getTipoDestinatarioArrayData,
+  getDependenciaGrupoArrayData,
   getSedeAdministrativaArrayData,
-  getDependenciaGrupoArrayData
+  getTipoDestinatarioArrayData
 } from 'app/infrastructure/state-management/constanteDTO-state/constanteDTO-selectors';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-datos-destinatario',
@@ -17,19 +18,36 @@ import {
 })
 export class DatosDestinatarioComponent implements OnInit {
 
+  form: FormGroup;
+  tipoDestinatarioControl: AbstractControl;
+  sedeAdministrativaControl: AbstractControl;
+  dependenciaGrupoControl: AbstractControl;
+
 
   tipoDestinatarioSuggestions$: Observable<ConstanteDTO[]>;
   sedeAdministrativaSuggestions$: Observable<ConstanteDTO[]>;
   dependenciaGrupoSuggestions$: Observable<ConstanteDTO[]>;
 
 
-  constructor(private _store: Store<State>, private _sandbox: Sandbox) {
+  constructor(private _store: Store<State>, private _sandbox: Sandbox, private formBuilder: FormBuilder) {
+    this.initForm();
   }
 
   ngOnInit(): void {
     this.tipoDestinatarioSuggestions$ = this._store.select(getTipoDestinatarioArrayData);
     this.sedeAdministrativaSuggestions$ = this._store.select(getSedeAdministrativaArrayData);
     this.dependenciaGrupoSuggestions$ = this._store.select(getDependenciaGrupoArrayData);
+  }
+
+  initForm() {
+    this.tipoDestinatarioControl = new FormControl(null, Validators.required);
+    this.sedeAdministrativaControl = new FormControl(null, Validators.required);
+    this.dependenciaGrupoControl = new FormControl(null, Validators.required);
+    this.form = this.formBuilder.group({
+      'tipoDestinatario': this.tipoDestinatarioControl,
+      'sedeAdministrativa': this.sedeAdministrativaControl,
+      'dependenciaGrupo': this.dependenciaGrupoControl,
+    });
   }
 
   onFilterTipoDestinatario($event) {
--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/bussiness-components/datos-remitente/datos-remitente.component.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/bussiness-components/datos-remitente/datos-remitente.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {ConstanteDTO} from 'app/domain/constanteDTO';
 import {Store} from '@ngrx/store';
@@ -10,6 +10,7 @@ import {
   getTipoTelefonoArrayData,
   getTratamientoCortesiaArrayData
 } from 'app/infrastructure/state-management/constanteDTO-state/constanteDTO-selectors';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -18,12 +19,28 @@ import {
 })
 export class DatosRemitenteComponent implements OnInit {
 
+  form: FormGroup;
+  tipoPersonaControl: AbstractControl;
+  nitControl: AbstractControl;
+  actuaCalidadControl: AbstractControl;
+  tipoDocumentoControl: AbstractControl;
+  razonSocialControl: AbstractControl;
+  nombreApellidosControl: AbstractControl;
+  tipoTelefonoControl: AbstractControl;
+  inactivoControl: AbstractControl;
+  numeroTelControl: AbstractControl;
+  correoEleControl: AbstractControl;
+  paisControl: AbstractControl;
+  departamentoControl: AbstractControl;
+  municipioControl: AbstractControl;
+
   tipoTelefonoSuggestions$: Observable<ConstanteDTO[]>;
   tipoPersonaSuggestions$: Observable<ConstanteDTO[]>;
   tipoDocumentoSuggestons$: Observable<ConstanteDTO[]>;
   tratamientoCortesiaSuggestons$: Observable<ConstanteDTO[]>;
 
-  constructor(private _store: Store<State>, private _sandbox: Sandbox) {
+  constructor(private _store: Store<State>, private _sandbox: Sandbox, private formBuilder: FormBuilder) {
+    this.initForm();
   }
 
   ngOnInit(): void {
@@ -31,6 +48,38 @@ export class DatosRemitenteComponent implements OnInit {
     this.tipoPersonaSuggestions$ = this._store.select(getTipoPersonaArrayData);
     this.tipoDocumentoSuggestons$ = this._store.select(getTipoDocumentoArrayData);
     this.tratamientoCortesiaSuggestons$ = this._store.select(getTratamientoCortesiaArrayData);
+  }
+
+  initForm() {
+    this.tipoPersonaControl = new FormControl(null);
+    this.nitControl = new FormControl(null);
+    this.actuaCalidadControl = new FormControl(null);
+    this.tipoDocumentoControl = new FormControl(null);
+    this.razonSocialControl = new FormControl(null);
+    this.nombreApellidosControl = new FormControl(null, Validators.required);
+    this.tipoTelefonoControl = new FormControl(null);
+    this.inactivoControl = new FormControl(null);
+    this.numeroTelControl = new FormControl(null);
+    this.correoEleControl = new FormControl(null);
+    this.paisControl = new FormControl(null);
+    this.departamentoControl = new FormControl(null);
+    this.municipioControl = new FormControl(null);
+
+    this.form = this.formBuilder.group({
+      'tipoPersona': this.tipoPersonaControl,
+      'nit': this.nitControl,
+      'actuaCalidad': this.actuaCalidadControl,
+      'tipoDocumento': this.tipoDocumentoControl,
+      'razonSocial': this.razonSocialControl,
+      'nombreApellidos': this.nombreApellidosControl,
+      'tipoTelefono': this.tipoTelefonoControl,
+      'inactivo': this.inactivoControl,
+      'numeroTel': this.numeroTelControl,
+      'correoEle': this.correoEleControl,
+      'pais': this.paisControl,
+      'departamento': this.departamentoControl,
+      'municipio': this.municipioControl,
+    });
   }
 
   onFilterTipoTelefono($event) {
