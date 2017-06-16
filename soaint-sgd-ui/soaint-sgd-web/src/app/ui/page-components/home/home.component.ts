80f2b6169c58ebd20e66d53be071a03426c812c7
Daniel Barrios <dbarrios1907@gmail.com>
2017-06-16 20:18:51 +0200

    incluyendo datos vinculados de los procesos y las tareas del bpm

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/home/home.component.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/home/home.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {Sandbox as ProcessDtoSandbox} from 'app/infrastructure/state-management/procesoDTO-state/procesoDTO-sandbox';
+import {Sandbox as TaskDtoSandbox} from 'app/infrastructure/state-management/tareasDTO-state/tareasDTO-sandbox';
 import {Observable} from 'rxjs/Observable';
 import {State as RootState} from 'app/infrastructure/redux-store/redux-reducers';
 import {Store} from '@ngrx/store';
-import {getArrayData as ProcessArrayData} from '../../../infrastructure/state-management/procesoDTO-state/procesoDTO-selectors';
+import {getArrayData as ProcessArrayData} from 'app/infrastructure/state-management/procesoDTO-state/procesoDTO-selectors';
+import {getArrayData as TaskArrayData} from 'app/infrastructure/state-management/tareasDTO-state/tareasDTO-selectors';
 
 
 @Component({
@@ -12,11 +14,15 @@ import {getArrayData as ProcessArrayData} from '../../../infrastructure/state-ma
 })
 export class HomeComponent implements OnInit {
 
-  allProcess$: Observable<any[]>;
+  bpmProcess$: Observable<any[]>;
 
-  constructor(private _store: Store<RootState>, private _processSandbox: ProcessDtoSandbox) {
+  bpmTasks$: Observable<any[]>;
 
-    this.allProcess$ = this._store.select(ProcessArrayData);
+  constructor(private _store: Store<RootState>, private _processSandbox: ProcessDtoSandbox, private _taskSandbox: TaskDtoSandbox) {
+
+    this.bpmProcess$ = this._store.select(ProcessArrayData);
+    this.bpmTasks$ = this._store.select(TaskArrayData);
+
   }
 
   ngOnInit() {
