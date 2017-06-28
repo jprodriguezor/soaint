2709abf64ae8418eb06583c407944d11f5517827
leinier <leinieralvarez@gmail.com>
2017-06-28 16:48:03 -0400

    Obtener funcionario api gateway

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/asignacion-comunicaciones/asignacion-comunicaciones.component.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/asignacion-comunicaciones/asignacion-comunicaciones.component.ts
@@ -10,11 +10,27 @@ export class AsignarComunicacionesComponent implements OnInit {
 
   comunicaciones: Array<ComunicacionOficialDTO> = [];
 
+  estadosCorrespondencia: [{ label: string, value: string }];
+
+  start_date: Date = new Date();
+
+  end_date: Date = new Date();
+
   constructor(private _comunicacionOficial: ComunicacionApiService) {
+    this.start_date.setHours(this.start_date.getHours() - 24);
   }
 
   ngOnInit() {
+    this.llenarEstadosCorrespondencias();
+  }
 
+  llenarEstadosCorrespondencias() {
+    this.estadosCorrespondencia = [
+      {
+        label: 'SIN ASIGNAR',
+        value: 'SIN_ASIGNAR'
+      }
+    ]
   }
 
   listarComunicaciones() {
