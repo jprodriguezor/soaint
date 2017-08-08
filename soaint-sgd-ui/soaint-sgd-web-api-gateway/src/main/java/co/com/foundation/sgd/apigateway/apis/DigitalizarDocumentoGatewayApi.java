32a9aa78b3aae72c1fb9cf597c7bea8c1d9f8f3e
Daniel Barrios <dbarrios1907@gmail.com>
2017-08-08 14:37:54 +0200

    Clase gateway digitalizar documento

---
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/DigitalizarDocumentoGatewayApi.java
@@ -0,0 +1,36 @@
+package co.com.foundation.sgd.apigateway.apis;
+
+import co.com.foundation.sgd.apigateway.apis.delegator.ActuaCalidadClient;
+import co.com.foundation.sgd.apigateway.security.annotations.JWTTokenSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/digitalizar-documento-gateway-api")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class DigitalizarDocumentoGatewayApi {
+
+
+    public DigitalizarDocumentoGatewayApi() {
+        super();
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
+
+    @GET
+    @Path("/")
+    @JWTTokenSecurity
+    public Response list() {
+        //TODO: add trafic log
+        System.out.println("ActuaCalidadGatewayApi - [trafic] - listing ActuaCalidad");
+
+        return Response.ok().build();
+    }
+
+}
