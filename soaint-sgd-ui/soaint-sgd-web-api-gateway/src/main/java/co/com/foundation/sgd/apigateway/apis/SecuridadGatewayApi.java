83c61e950e1777e15aadb425fc8feface67a18aa
Leinier Alvarez Armas <leinieralvarez@gmail.com>
2017-08-14 13:26:54 -0400

    Externalizando webservice endpoint URL

--- soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/SecuridadGatewayApi.java
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/SecuridadGatewayApi.java
@@ -5,6 +5,8 @@ import co.com.foundation.sgd.apigateway.webservice.proxy.securitycardbridge.Auth
 import co.com.foundation.sgd.dto.AccountDTO;
 import co.com.foundation.sgd.infrastructure.KeyManager;
 import co.com.soaint.foundation.canonical.seguridad.UsuarioDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -26,8 +28,12 @@ public class SecuridadGatewayApi {
     @Context
     private UriInfo uriInfo;
 
+    @Autowired
+    private SecurityCardbridgeClient securityCardbridgeClient;
+
     public SecuridadGatewayApi() {
         super();
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
     }
 
     @POST
@@ -37,8 +43,9 @@ public class SecuridadGatewayApi {
         try {
             // Authenticate the user using the credentials provided
             System.out.println("calling login service");
+            System.out.println(securityCardbridgeClient);
 
-            AuthenticationResponseContext context = SecurityCardbridgeClient.verifyCredentials(user.getLogin(), user.getPassword());
+            AuthenticationResponseContext context = securityCardbridgeClient.verifyCredentials(user.getLogin(), user.getPassword());
 
             if (context.isSuccessful()) {
                 // Issue a token for the user
