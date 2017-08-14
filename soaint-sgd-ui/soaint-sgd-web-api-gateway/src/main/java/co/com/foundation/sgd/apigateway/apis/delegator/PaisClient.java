01a330d15f76fa1a34f51c47595fa774ce9c66ce
Jorge <jorge.softdevelop@gmail.com>
2017-08-14 15:56:38 -0400

    MOD: nuevos ajustes con lombok para log4j

--- soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/delegator/PaisClient.java
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/delegator/PaisClient.java
@@ -1,6 +1,7 @@
 package co.com.foundation.sgd.apigateway.apis.delegator;
 
 import co.com.foundation.sgd.infrastructure.ApiDelegator;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.ws.rs.client.ClientBuilder;
@@ -8,6 +9,7 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
 @ApiDelegator
+@Log4j2
 public class PaisClient {
 
     @Value("${backapi.endpoint.url}")
@@ -18,7 +20,7 @@ public class PaisClient {
     }
 
     public Response list() {
-        System.out.println("Pais - [trafic] - listing Pais with endpoint: " + endpoint);
+        log.info("Pais - [trafic] - listing Pais with endpoint: " + endpoint);
         WebTarget wt = ClientBuilder.newClient().target(endpoint);
         return wt.path("/paises-web-api/paises/A")
                 .request()
