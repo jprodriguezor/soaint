fcdd8ed39caca326207608b63bd330e5ccdcc1f8
Ernesto Sanchez Aliaga <esanchez7802@gmail.com>
2017-08-15 10:45:50 -0400

    MOD: Fix logger in Test

--- soaint-sgd-enterprise-services/correspondencia-business-services/src/test/java/co/com/soaint/correspondencia/JPAHibernateTest.java
+++ soaint-sgd-enterprise-services/correspondencia-business-services/src/test/java/co/com/soaint/correspondencia/JPAHibernateTest.java
@@ -1,7 +1,6 @@
 package co.com.soaint.correspondencia;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.h2.tools.RunScript;
 import org.hibernate.Session;
 import org.hibernate.jdbc.Work;
@@ -13,24 +12,20 @@ import org.junit.Test;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.PersistenceException;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.sql.Connection;
-import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Jorge on 15/05/2017.
  */
-
+@Log4j2
 public class JPAHibernateTest {
 
     protected static EntityManagerFactory emf;
     protected static EntityManager em;
-    private static Logger logger = LogManager.getLogger(JPAHibernateTest.class.getName());
 
     @BeforeClass
     public static void init() {
@@ -48,7 +43,7 @@ public class JPAHibernateTest {
                     File script = new File(getClass().getResource("/data.sql").getFile());
                     RunScript.execute(connection, new FileReader(script));
                 }catch (Exception ex){
-                    logger.error("Test - a system error has occurred", ex);
+                    log.error("Test - a system error has occurred", ex);
                 }
 
             }
