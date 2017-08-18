2bdb8539bc334f4294b7b5d58063923c27cac448
Dasiel Otero Dartayet <dasiel@gmail.com>
2017-08-18 17:42:18 -0400

    FIX Code issues

--- soaint-sgd-enterprise-services/ecm-integration-services/src/main/java/co/com/soaint/ecm/business/boundary/documentmanager/interfaces/ContentControl.java
+++ soaint-sgd-enterprise-services/ecm-integration-services/src/main/java/co/com/soaint/ecm/business/boundary/documentmanager/interfaces/ContentControl.java
@@ -19,34 +19,38 @@ import java.util.List;
 public interface ContentControl {
 
     /**
-     *Obtener objeto conexion
+     * Obtener objeto conexion
+     *
      * @return Objeto conexion
      */
     Conexion obtenerConexion();
 
     /**
-     *MOver documento
-     * @param session objeto conexion
-     * @param documento nombre de documento
-     * @param carpetaFuente carpeta fuente
+     * MOver documento
+     *
+     * @param session        objeto conexion
+     * @param documento      nombre de documento
+     * @param carpetaFuente  carpeta fuente
      * @param carpetaDestino carpeta destino
      * @return mensaje respuesta
      */
     MensajeRespuesta movDocumento(Session session, String documento, String carpetaFuente, String carpetaDestino);
 
     /**
-     *Generar estructura
+     * Generar estructura
+     *
      * @param estructuraList lista de estructura
-     * @param folder carpeta padre
+     * @param folder         carpeta padre
      * @return mensaje respuesta
      */
     MensajeRespuesta generarArbol(List <EstructuraTrdDTO> estructuraList, Carpeta folder);
 
     /**
-     *Subir documento
-     * @param session Objeto conexion
-     * @param nombreDocumento nombre de documento
-     * @param documento documento a subir
+     * Subir documento
+     *
+     * @param session          Objeto conexion
+     * @param nombreDocumento  nombre de documento
+     * @param documento        documento a subir
      * @param tipoComunicacion tipo de comunicacion
      * @return ide de documento
      * @throws IOException exception
@@ -55,10 +59,11 @@ public interface ContentControl {
 
     /**
      * Descargar documento
+     *
      * @param idDocumento Identificador del documento en el ECM
-     * @param session Objeto conexion
+     * @param session     Objeto conexion
      * @return Se retorna el documento
      */
-    Response descargarDocumento(String idDocumento, Session session ) ;
+    Response descargarDocumento(String idDocumento, Session session);
 
 }
--- soaint-sgd-enterprise-services/ecm-integration-services/src/main/java/co/com/soaint/ecm/business/boundary/mediator/EcmManager.java
+++ soaint-sgd-enterprise-services/ecm-integration-services/src/main/java/co/com/soaint/ecm/business/boundary/mediator/EcmManager.java
@@ -7,14 +7,12 @@ import co.com.soaint.foundation.framework.exceptions.InfrastructureException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
-import org.jboss.resteasy.specimpl.ResponseBuilderImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import java.util.List;
-
 
 
 /**
@@ -31,6 +29,7 @@ public class EcmManager {
 
     /**
      * Metodo que llama el servicio para crear la estructura del ECM
+     *
      * @param structure Objeto que contiene la estrucutra a crear en el ECM
      * @return Mensaje de respuesta(codigo y mensaje)
      * @throws InfrastructureException Excepcion ante errores del metodo
@@ -44,7 +43,7 @@ public class EcmManager {
         } catch (Exception e) {
             response.setCodMensaje ("000005");
             response.setMensaje ("Error al crear estructura");
-            logger.error ("### Error Creando estructura content..------" , e);
+            logger.error ("### Error Creando estructura content..------", e);
         }
 
         return response;
@@ -52,19 +51,20 @@ public class EcmManager {
 
     /**
      * Metodo que llama el servicio para subir documentos al ECM
-     * @param nombreDocumento Nombre del documento a subir
-     * @param documento Documento que se va a subir
+     *
+     * @param nombreDocumento  Nombre del documento a subir
+     * @param documento        Documento que se va a subir
      * @param tipoComunicacion TIpo de comunicacion que puede ser Externa o Interna
      * @return Identificador del documento creado
      * @throws InfrastructureException Excepcion ante errores del metodo
      */
-    public String subirDocumento(String nombreDocumento, MultipartFormDataInput documento, String tipoComunicacion)  {
+    public String subirDocumento(String nombreDocumento, MultipartFormDataInput documento, String tipoComunicacion) {
         logger.info ("### Subiendo documento al content..");
         String idDocumento = "";
         try {
             idDocumento = contentManager.subirDocumentoContent (nombreDocumento, documento, tipoComunicacion);
         } catch (Exception e) {
-            logger.error ("### Error..------" , e);
+            logger.error ("### Error..------", e);
         }
 
         return idDocumento;
@@ -72,13 +72,14 @@ public class EcmManager {
 
     /**
      * Metodo que llama el servicio para mover documentos dentro del ECM
-     * @param documento Nombre del documento a mover
-     * @param carpetaFuente Carpeta donde se encuentra el documento
+     *
+     * @param documento      Nombre del documento a mover
+     * @param carpetaFuente  Carpeta donde se encuentra el documento
      * @param carpetaDestino Carpeta a donde se va a mover el documento.
      * @return Mensaje de respuesta (codigo y mensaje)
      * @throws InfrastructureException Excepcion ante errores del metodo
      */
-    public MensajeRespuesta moverDocumento(String documento, String carpetaFuente, String carpetaDestino){
+    public MensajeRespuesta moverDocumento(String documento, String carpetaFuente, String carpetaDestino) {
         logger.info ("### Moviendo documento dentro del content..");
         MensajeRespuesta response = new MensajeRespuesta ( );
         try {
@@ -88,7 +89,7 @@ public class EcmManager {
         } catch (Exception e) {
             response.setCodMensaje ("000002");
             response.setMensaje ("Error al mover documento");
-            logger.error ("### Error..------" , e);
+            logger.error ("### Error..------", e);
         }
 
         return response;
@@ -100,16 +101,16 @@ public class EcmManager {
      * @param idDoc Identificador del documento dentro del ECM
      * @return Documento
      */
-    public Response descargarDocumento(String idDoc){
+    public Response descargarDocumento(String idDoc) {
         logger.info ("### Descargando documento del content..");
-        ResponseBuilder response=new com.sun.jersey.core.spi.factory.ResponseBuilderImpl ();
-        try{
-            response=Response.ok(contentManager.descargarDocumentoContent (idDoc));
-        }catch(Exception e){
-        logger.error("Error descargando documento",e);
+        ResponseBuilder response = new com.sun.jersey.core.spi.factory.ResponseBuilderImpl ( );
+        try {
+            response = Response.ok (contentManager.descargarDocumentoContent (idDoc));
+        } catch (Exception e) {
+            logger.error ("Error descargando documento", e);
         }
         logger.info ("### Se devuelve el documento del content..");
-        return response.build ();
+        return response.build ( );
     }
 
 }
