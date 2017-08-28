324806bfe662cfefec76ddd8b96030e4ed428506
Jorge <jorge.softdevelop@gmail.com>
2017-08-28 17:18:43 -0400

    MOD: ajuste en la generacion del cliente del servicio desde maven, revisar el plugin incluido en el pom.xml. Se debe buscar la ruta en target/generated-sources y marcarla como generated source root para que el cliente vea las clases generadas. El WSDL del servicio debe ponerse en resources

--- soaint-sgd-enterprise-services/funcionario-integration-services/src/main/java/co/com/soaint/funcionario/apis/delegator/security/SecurityApiClient.java
+++ soaint-sgd-enterprise-services/funcionario-integration-services/src/main/java/co/com/soaint/funcionario/apis/delegator/security/SecurityApiClient.java
@@ -1,7 +1,7 @@
 package co.com.soaint.funcionario.apis.delegator.security;
 
-import co.com.soaint.funcionario.apis.delegator.security.wsclient.SecurityAPIService;
 import co.com.soaint.funcionario.infrastructure.ApiDelegator;
+import com.soaint.services.security_cartridge._1_0.SecurityAPIService;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -10,18 +10,20 @@ import java.net.URL;
 /**
  * Created by esanchez on 8/28/2017.
  */
-@ApiDelegator
-@Log4j2
-@NoArgsConstructor
+//@ApiDelegator
+//@Log4j2
+//@NoArgsConstructor
 public class SecurityApiClient {
     public static void main(String[] args){
+
         try {
             URL url = new URL("http://192.168.3.242:28080/Security-Cartridge/SecurityAPI?wsdl");
             SecurityAPIService securityApiService = new SecurityAPIService(url);
-            System.out.print(securityApiService.getSecurityAPIPort().listadoDeRoles());
+            System.out.print("Autenticacion: " + securityApiService.getSecurityAPIPort().verifyCredentials("jorge.infante","descarga*22").isSuccessful());
         }
         catch (Exception e){
             e.printStackTrace();
         }
+
     }
 }
--- soaint-sgd-enterprise-services/funcionario-integration-services/src/main/java/co/com/soaint/funcionario/apis/delegator/security/wsclient/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.soaint.com/services/security-cartridge/1.0.0")
-package co.com.soaint.funcionario.apis.delegator.security.wsclient;
