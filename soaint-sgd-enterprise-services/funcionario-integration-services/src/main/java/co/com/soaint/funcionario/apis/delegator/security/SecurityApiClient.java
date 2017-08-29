315bfcb82436672952b6bcf5c5406a14b42c319d
Ernesto Sanchez Aliaga <esanchez7802@gmail.com>
2017-08-29 10:33:13 -0400

    MOD: Funcionarios integration service

--- soaint-sgd-enterprise-services/funcionario-integration-services/src/main/java/co/com/soaint/funcionario/apis/delegator/security/SecurityApiClient.java
+++ soaint-sgd-enterprise-services/funcionario-integration-services/src/main/java/co/com/soaint/funcionario/apis/delegator/security/SecurityApiClient.java
@@ -1,29 +1,56 @@
 package co.com.soaint.funcionario.apis.delegator.security;
 
+import co.com.soaint.foundation.canonical.correspondencia.FuncionarioDTO;
+import co.com.soaint.foundation.canonical.correspondencia.RolDTO;
+import co.com.soaint.foundation.framework.components.util.ExceptionBuilder;
+import co.com.soaint.foundation.framework.exceptions.SystemException;
 import co.com.soaint.funcionario.infrastructure.ApiDelegator;
 import com.soaint.services.security_cartridge._1_0.SecurityAPIService;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by esanchez on 8/28/2017.
  */
-//@ApiDelegator
-//@Log4j2
-//@NoArgsConstructor
+@ApiDelegator
+@Log4j2
+@NoArgsConstructor
 public class SecurityApiClient {
-    public static void main(String[] args){
 
+    @Value("${securityapi.endpoint.url}")
+    private String endpoint = "";
+
+    /**
+     *
+     * @param rol
+     * @return
+     * @throws SystemException
+     */
+    public List<FuncionarioDTO> listarUsusriosByRol(String rol)throws SystemException{
+        List<FuncionarioDTO> funcionarios = new ArrayList<>();
         try {
-            URL url = new URL("http://192.168.3.242:28080/Security-Cartridge/SecurityAPI?wsdl");
-            SecurityAPIService securityApiService = new SecurityAPIService(url);
-            System.out.print("Autenticacion: " + securityApiService.getSecurityAPIPort().verifyCredentials("jorge.infante","descarga*22").isSuccessful());
+            SecurityAPIService securityApiService = new SecurityAPIService(new URL(endpoint));
+            securityApiService.getSecurityAPIPort().obtenerUsuariosporRol(rol).getUsuarios().getUsuario()
+                    .stream().forEach(usuario -> {
+                FuncionarioDTO funcionario = FuncionarioDTO.newInstance()
+                        .loginName(usuario.getUsername())
+                        .roles(new ArrayList<>())
+                        .build();
+                usuario.getRoles().getRol().stream().forEach(uRol -> funcionario.getRoles().add(RolDTO.newInstance().rol(uRol.getName()).build()));
+                funcionarios.add(funcionario);
+            });
+            return funcionarios;
+        } catch (Exception ex) {
+            log.error("Api Delegator - a system error has occurred", ex);
+            throw ExceptionBuilder.newBuilder()
+                    .withMessage("system.generic.error")
+                    .withRootException(ex)
+                    .buildSystemException();
         }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
     }
 }
--- soaint-sgd-enterprise-services/funcionario-integration-services/src/main/java/co/com/soaint/funcionario/business/control/FuncionariosControl.java
+++ soaint-sgd-enterprise-services/funcionario-integration-services/src/main/java/co/com/soaint/funcionario/business/control/FuncionariosControl.java
@@ -1,7 +1,9 @@
 package co.com.soaint.funcionario.business.control;
 
+import co.com.soaint.foundation.canonical.correspondencia.FuncionarioDTO;
 import co.com.soaint.foundation.canonical.correspondencia.FuncionariosDTO;
 import co.com.soaint.foundation.framework.annotations.BusinessControl;
+import co.com.soaint.foundation.framework.components.util.ExceptionBuilder;
 import co.com.soaint.foundation.framework.exceptions.SystemException;
 import co.com.soaint.funcionario.apis.delegator.funcionarios.FuncionariosWebApiClient;
 import co.com.soaint.funcionario.apis.delegator.security.SecurityApiClient;
@@ -42,11 +44,22 @@ public class FuncionariosControl {
                 .newInstance()
                 .funcionarios(new ArrayList<>())
                 .build();
-        List<String> usuariosRol = securityApiClient.listarUsusriosByRol(rol);
-        funcionariosWebApiClient.listarFuncionariosByDependenciaAndEstado(codDependencia, codEstado).getFuncionarios().stream().forEach(funcionario -> {
-            if(usuariosRol.stream().anyMatch(x -> x.equals(funcionario.getLoginName())))
-                funcionariosDTO.getFuncionarios().add(funcionario);
-        });
-        return funcionariosDTO;
+        try {
+            List<FuncionarioDTO> usuariosRol = securityApiClient.listarUsusriosByRol(rol);
+            funcionariosWebApiClient.listarFuncionariosByDependenciaAndEstado(codDependencia, codEstado).getFuncionarios().stream().forEach(funcionario -> {
+                FuncionarioDTO usuarioRol = usuariosRol.stream().filter(x -> x.getLoginName().equals(funcionario.getLoginName())).findFirst().get();
+                if (usuarioRol != null) {
+                    funcionario.setRoles(usuarioRol.getRoles());
+                    funcionariosDTO.getFuncionarios().add(funcionario);
+                }
+            });
+            return funcionariosDTO;
+        } catch (Exception ex) {
+            log.error("Business Control - a system error has occurred", ex);
+            throw ExceptionBuilder.newBuilder()
+                    .withMessage("system.generic.error")
+                    .withRootException(ex)
+                    .buildSystemException();
+        }
     }
 }
