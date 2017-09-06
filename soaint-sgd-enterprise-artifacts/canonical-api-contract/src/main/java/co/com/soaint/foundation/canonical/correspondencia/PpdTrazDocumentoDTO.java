ef4be766cfe746e5d4576c6a1f7a923adb8cd0a7
Ernesto Sanchez Aliaga <esanchez7802@gmail.com>
2017-09-06 11:48:10 -0400

    MOD: Registrar y listar observaciones

--- soaint-sgd-enterprise-artifacts/canonical-api-contract/src/main/java/co/com/soaint/foundation/canonical/correspondencia/PpdTrazDocumentoDTO.java
+++ soaint-sgd-enterprise-artifacts/canonical-api-contract/src/main/java/co/com/soaint/foundation/canonical/correspondencia/PpdTrazDocumentoDTO.java
@@ -38,12 +38,14 @@ public class PpdTrazDocumentoDTO implements Serializable {
     @JsonProperty("estado")
     private String codEstado;
     private BigInteger ideDocumento;
+    @JsonProperty("codDependencia")
     private String codOrgaAdmin;
     private String nomFuncionario;
     private String valApellido1;
     private String valApellido2;
     private String corrElectronico;
     private String loginName;
+    private String nomDependencia;
 
     public PpdTrazDocumentoDTO(BigInteger ideTrazDocumento, Date fecTrazDocumento, String observacion, BigInteger ideFunci,
                                String codEstado, BigInteger ideDocumento, String codOrgaAdmin){
@@ -54,5 +56,22 @@ public class PpdTrazDocumentoDTO implements Serializable {
         this.codEstado = codEstado;
         this.ideDocumento = ideDocumento;
         this.codOrgaAdmin = codOrgaAdmin;
+    }
+
+    public PpdTrazDocumentoDTO(BigInteger ideTrazDocumento, Date fecTrazDocumento, String observacion, BigInteger ideFunci,
+                               String codEstado, BigInteger ideDocumento, String codOrgaAdmin, String nomFuncionario,
+                               String valApellido1, String valApellido2, String corrElectronico, String loginName){
+        this.ideTrazDocumento = ideTrazDocumento;
+        this.fecTrazDocumento = fecTrazDocumento;
+        this.observacion = observacion;
+        this.ideFunci = ideFunci;
+        this.codEstado = codEstado;
+        this.ideDocumento = ideDocumento;
+        this.codOrgaAdmin = codOrgaAdmin;
+        this.nomFuncionario = nomFuncionario;
+        this.valApellido1 = valApellido1;
+        this.valApellido2 = valApellido2;
+        this.corrElectronico = corrElectronico;
+        this.loginName = loginName;
     }
 }
