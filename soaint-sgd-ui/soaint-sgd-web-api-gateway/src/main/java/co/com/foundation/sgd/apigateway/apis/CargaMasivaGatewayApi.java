725e3573f7294e85ce06312d62b0dbbdaa995f9c
Dasiel Otero Dartayet <dasiel@gmail.com>
2017-09-07 13:35:04 -0400

    MOD Adicionando metodo para la clase proxy

--- soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/CargaMasivaGatewayApi.java
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/CargaMasivaGatewayApi.java
@@ -1,4 +1,63 @@
 package co.com.foundation.sgd.apigateway.apis;
 
+import co.com.foundation.sgd.apigateway.apis.delegator.CargaMasivaClient;
+import co.com.foundation.sgd.apigateway.security.annotations.JWTTokenSecurity;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/Massive-Loader")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Log4j2
 public class CargaMasivaGatewayApi {
+
+    @Autowired
+    private CargaMasivaClient client;
+
+    public CargaMasivaGatewayApi() {
+        super();
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
+
+    @GET
+    @Path("/listadocargamasiva")
+    @JWTTokenSecurity
+    public Response listCargaMasiva() {
+        log.info("CargaMasivaGatewayApi - [trafic] - listing Carga Masiva");
+        Response response = client.listCargaMasiva ();
+        String responseContent = response.readEntity(String.class);
+        log.info("CargaMasivaGatewayApi - [content] : " + responseContent);
+
+        return Response.status( response.getStatus() ).entity(responseContent).build();
+    }
+
+    @GET
+    @Path("/estadocargamasiva")
+    @JWTTokenSecurity
+    public Response listEstadoCargaMasiva() {
+        log.info("CargaMasivaGatewayApi - [trafic] - listing Estado Carga Masiva");
+        Response response = client.listEstadoCargaMasiva ();
+        String responseContent = response.readEntity(String.class);
+        log.info("CargaMasivaGatewayApi - [content] : " + responseContent);
+
+        return Response.status( response.getStatus() ).entity(responseContent).build();
+    }
+
+    @GET
+    @Path("/estadocargamasiva/{id}")
+    @JWTTokenSecurity
+    public Response listEstadoCargaMasivaDadoId(@PathParam("id") String id) {
+        log.info("CargaMasivaGatewayApi - [trafic] - listing Estado Carga Masiva dado Id");
+        Response response = client.listEstadoCargaMasivaDadoId (id);
+        String responseContent = response.readEntity(String.class);
+        log.info("CargaMasivaGatewayApi - [content] : " + responseContent);
+
+        return Response.status( response.getStatus() ).entity(responseContent).build();
+    }
 }
+
--- soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/delegator/CargaMasivaClient.java
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/delegator/CargaMasivaClient.java
@@ -1,4 +1,44 @@
 package co.com.foundation.sgd.apigateway.apis.delegator;
 
+import co.com.foundation.sgd.infrastructure.ApiDelegator;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+@ApiDelegator
+@Log4j2
 public class CargaMasivaClient {
+    @Value("${backapi.cargamasiva.endpoint.url}")
+    private String endpoint = "";
+
+    public CargaMasivaClient() {
+        super();
+    }
+
+    public Response listCargaMasiva() {
+        log.info("Carga Masiva - [trafic] - listing Carga Masiva with endpoint: " + endpoint);
+        WebTarget wt = ClientBuilder.newClient().target(endpoint);
+        return wt.path("/listadocargamasiva")
+                .request()
+                .get();
+    }
+
+    public Response listEstadoCargaMasiva() {
+        log.info("Carga Masiva - [trafic] - listing Carga Masiva with endpoint: " + endpoint);
+        WebTarget wt = ClientBuilder.newClient().target(endpoint);
+        return wt.path("/estadocargamasiva")
+                .request()
+                .get();
+    }
+
+    public Response listEstadoCargaMasivaDadoId(String id) {
+        log.info("Carga Masiva - [trafic] - listing Carga Masiva dado Id with endpoint: " + endpoint);
+        WebTarget wt = ClientBuilder.newClient().target(endpoint);
+        return wt.path("/estadocargamasiva/"+id)
+                .request()
+                .get();
+    }
 }
