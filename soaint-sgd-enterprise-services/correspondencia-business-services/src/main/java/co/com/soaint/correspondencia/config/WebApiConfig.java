22713194e8abbf9904e1a9474f8bdec19b0279cf
Ernesto Sanchez Aliaga <esanchez7802@gmail.com>
2017-09-15 16:42:54 -0400

    MOD: Api configuration

--- soaint-sgd-enterprise-services/correspondencia-business-services/src/main/java/co/com/soaint/correspondencia/config/WebApiConfig.java
+++ soaint-sgd-enterprise-services/correspondencia-business-services/src/main/java/co/com/soaint/correspondencia/config/WebApiConfig.java
@@ -14,13 +14,17 @@ import co.com.soaint.correspondencia.integration.service.rest.*;
 import io.swagger.jaxrs.config.BeanConfig;
 import io.swagger.jaxrs.listing.ApiListingResource;
 import io.swagger.jaxrs.listing.SwaggerSerializers;
+import lombok.extern.log4j.Log4j2;
 
+import javax.management.ObjectName;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.lang.management.ManagementFactory;
 import java.util.HashSet;
 import java.util.Set;
 
 @ApplicationPath("/services")
+@Log4j2
 public class WebApiConfig extends Application {
     public WebApiConfig() {
         //Init Rest Api
@@ -28,15 +32,23 @@ public class WebApiConfig extends Application {
     }
 
     private void initSwagger() {
-        BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setTitle("Probando uso de Swagger con JAX-RS en Wildfly");
-        beanConfig.setVersion("1.0");
-        beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setHost("192.168.3.242:28080");
-        beanConfig.setBasePath("/correspondencia-business-services/services");
-        beanConfig.setResourcePackage("co.com.soaint.correspondencia.integration.service.rest");
-        beanConfig.setScan(true);
+        try {
+            ObjectName socketBinding = new ObjectName("jboss.as:socket-binding-group=standard-sockets,socket-binding=http");
+            Integer port = (Integer) ManagementFactory.getPlatformMBeanServer().getAttribute(socketBinding, "port");
+            String host = (String) ManagementFactory.getPlatformMBeanServer().getAttribute(socketBinding, "boundAddress");
+            BeanConfig beanConfig = new BeanConfig();
+            beanConfig.setTitle("Correspondencia Business Services");
+            beanConfig.setVersion("1.0");
+            beanConfig.setSchemes(new String[]{"http"});
+            beanConfig.setHost(host + ":" + port);
+            beanConfig.setBasePath("/correspondencia-business-services/services");
+            beanConfig.setResourcePackage("co.com.soaint.correspondencia.integration.service.rest");
+            beanConfig.setScan(true);
+        } catch (Exception ex) {
+            log.error("Rest Api - a system error has occurred", ex);
+        }
     }
+
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new HashSet<>();
