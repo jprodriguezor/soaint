5c96ad5362b181acb9a251403bedea95f88161a5
Ernesto Tenorio <etenorio87@gmail.com>
2017-10-02 13:06:01 -0400

    ProdDoc: Componentes include

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/_pd-components.include.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/_pd-components.include.ts
@@ -3,6 +3,9 @@ import {PDDatosGeneralesComponent} from "./components/datos-generales/datos-gene
 import {PDRadicadoAsociadoComponent} from "./components/radicado-asociado/radicado-asociado.component";
 import {PDProducirDocumentoComponent} from "./components/producir-documento/producir-documento.component";
 import {PDAnexosComponent} from "./components/anexos/anexos.component";
+import {ProduccionDocumentalStep2Component} from "./produccion-documental-step2.component";
+import {ProduccionDocumentalStep3Component} from "./produccion-documental-step3.component";
+import {ProduccionDocumentalMultComponent} from "./produccion-documental-mult.component";
 
 export const PRODUCCION_DOCUMENTAL_COMPONENTS = [
   ProduccionDocumentalComponent,PDDatosGeneralesComponent,PDRadicadoAsociadoComponent,PDProducirDocumentoComponent,PDAnexosComponent
