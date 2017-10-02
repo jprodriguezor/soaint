efc1ed774596f93cbb714976636a6c24d93ea59b
Ernesto Sanchez Aliaga <esanchez7802@gmail.com>
2017-10-02 16:41:41 -0400

    MOD: Ajustes controladores por cambio modelo datos

--- soaint-sgd-enterprise-services/correspondencia-business-services/src/main/java/co/com/soaint/correspondencia/business/control/AgenteControl.java
+++ soaint-sgd-enterprise-services/correspondencia-business-services/src/main/java/co/com/soaint/correspondencia/business/control/AgenteControl.java
@@ -6,6 +6,7 @@ import co.com.soaint.foundation.canonical.correspondencia.*;
 import co.com.soaint.foundation.canonical.correspondencia.constantes.EstadoAgenteEnum;
 import co.com.soaint.foundation.canonical.correspondencia.constantes.EstadoCorrespondenciaEnum;
 import co.com.soaint.foundation.canonical.correspondencia.constantes.TipoAgenteEnum;
+import co.com.soaint.foundation.canonical.correspondencia.constantes.TipoRemitenteEnum;
 import co.com.soaint.foundation.framework.annotations.BusinessControl;
 import co.com.soaint.foundation.framework.components.util.ExceptionBuilder;
 import co.com.soaint.foundation.framework.exceptions.BusinessException;
@@ -46,14 +47,16 @@ public class AgenteControl {
     @Value("${radicado.max.num.redirecciones}")
     private int numMaxRedirecciones;
 
+    @Value("${radicado.req.dist.fisica}")
+    private String reqDistFisica;
+
     /**
-     *
      * @param ideDocumento
      * @return
      * @throws SystemException
      */
-    public List<AgenteDTO> listarRemitentesByIdeDocumento(BigInteger ideDocumento)throws SystemException{
-        try{
+    public List<AgenteDTO> listarRemitentesByIdeDocumento(BigInteger ideDocumento) throws SystemException {
+        try {
             return em.createNamedQuery("CorAgente.findByIdeDocumentoAndCodTipoAgente", AgenteDTO.class)
                     .setParameter("COD_TIP_AGENT", TipoAgenteEnum.REMITENTE.getCodigo())
                     .setParameter("IDE_DOCUMENTO", ideDocumento)
@@ -68,7 +71,6 @@ public class AgenteControl {
     }
 
     /**
-     *
      * @param ideDocumento
      * @param codDependencia
      * @param codEstado
@@ -76,15 +78,15 @@ public class AgenteControl {
      * @throws SystemException
      */
     public List<AgenteDTO> listarDestinatarioByIdeDocumentoAndCodDependenciaAndCodEstado(BigInteger ideDocumento,
-                                                                                   String codDependencia,
-                                                                                   String codEstado)throws SystemException{
-        try{
-        return em.createNamedQuery("CorAgente.findByIdeDocumentoAndCodDependenciaAndCodEstado", AgenteDTO.class)
-                .setParameter("COD_ESTADO", codEstado)
-                .setParameter("COD_DEPENDENCIA", codDependencia)
-                .setParameter("COD_TIP_AGENT", TipoAgenteEnum.DESTINATARIO.getCodigo())
-                .setParameter("IDE_DOCUMENTO", ideDocumento)
-                .getResultList();
+                                                                                         String codDependencia,
+                                                                                         String codEstado) throws SystemException {
+        try {
+            return em.createNamedQuery("CorAgente.findByIdeDocumentoAndCodDependenciaAndCodEstado", AgenteDTO.class)
+                    .setParameter("COD_ESTADO", codEstado)
+                    .setParameter("COD_DEPENDENCIA", codDependencia)
+                    .setParameter("COD_TIP_AGENT", TipoAgenteEnum.DESTINATARIO.getCodigo())
+                    .setParameter("IDE_DOCUMENTO", ideDocumento)
+                    .getResultList();
         } catch (Exception ex) {
             log.error("Business Control - a system error has occurred", ex);
             throw ExceptionBuilder.newBuilder()
@@ -95,14 +97,13 @@ public class AgenteControl {
     }
 
     /**
-     *
      * @param ideAgente
      * @return
      * @throws BusinessException
      * @throws SystemException
      */
-    public AgenteDTO consultarAgenteByIdeAgente(BigInteger ideAgente)throws BusinessException, SystemException{
-        try{
+    public AgenteDTO consultarAgenteByIdeAgente(BigInteger ideAgente) throws BusinessException, SystemException {
+        try {
             return em.createNamedQuery("CorAgente.findByIdeAgente", AgenteDTO.class)
                     .setParameter("IDE_AGENTE", ideAgente)
                     .getSingleResult();
@@ -162,25 +163,12 @@ public class AgenteControl {
      */
     public void redireccionarCorrespondencia(AgentesDTO agentesDTO) throws SystemException {
         try {
-            String codSede;
-            String codDependencia;
-
-            for (AgenteDTO agenteDTO : agentesDTO.getAgentes()) {
-                codSede = agenteDTO.getCodSede();
-                codDependencia = agenteDTO.getCodDependencia();
-                AsignacionDTO asignacion = asignacionControl.consultarAsignacionByIdeAgente(agenteDTO.getIdeAgente());
-                if (!puedeRedireccionar(asignacion)) {
-                    CorrespondenciaDTO correspondencia = correspondenciaControl.consultarCorrespondenciaByNroRadicado(asignacion.getNroRadicado());
-                    codSede = correspondencia.getCodSede();
-                    codDependencia = correspondencia.getCodDependencia();
-                }
-
+            for (AgenteDTO agenteDTO : agentesDTO.getAgentes())
                 em.createNamedQuery("CorAgente.redireccionarCorrespondencia")
-                        .setParameter("COD_SEDE", codSede)
-                        .setParameter("COD_DEPENDENCIA", codDependencia)
+                        .setParameter("COD_SEDE", agenteDTO.getCodSede())
+                        .setParameter("COD_DEPENDENCIA", agenteDTO.getCodDependencia())
                         .setParameter("IDE_AGENTE", agenteDTO.getIdeAgente())
                         .executeUpdate();
-            }
         } catch (Exception ex) {
             log.error("Business Control - a system error has occurred", ex);
             throw ExceptionBuilder.newBuilder()
@@ -225,6 +213,32 @@ public class AgenteControl {
     }
 
     /**
+     * @param agentes
+     * @param datosContactoList
+     * @param rDistFisica
+     * @return
+     */
+    public List<CorAgente> conformarAgentes(List<AgenteDTO> agentes, List<DatosContactoDTO> datosContactoList, String rDistFisica) {
+        long valorInicialRedirecciones = Long.parseLong("0");
+        List<CorAgente> corAgentes = new ArrayList<>();
+        for (AgenteDTO agenteDTO : agentes) {
+            CorAgente corAgente = corAgenteTransform(agenteDTO);
+
+            if (TipoAgenteEnum.REMITENTE.getCodigo().equals(agenteDTO.getCodTipAgent()) && TipoRemitenteEnum.EXTERNO.getCodigo().equals(agenteDTO.getCodTipoRemite()))
+                AgenteControl.asignarDatosContacto(corAgente, datosContactoList);
+
+            if (TipoAgenteEnum.DESTINATARIO.getCodigo().equals(agenteDTO.getCodTipAgent())) {
+                corAgente.setCodEstado(reqDistFisica.equals(rDistFisica) ? EstadoAgenteEnum.DISTRIBUCION.getCodigo() : EstadoAgenteEnum.SIN_ASIGNAR.getCodigo());
+                corAgente.setNumRedirecciones(valorInicialRedirecciones);
+                corAgente.setNumDevoluciones(valorInicialRedirecciones);
+            }
+
+            corAgentes.add(corAgente);
+        }
+        return corAgentes;
+    }
+
+    /**
      * @param agenteDTO
      * @return
      */
@@ -246,22 +260,9 @@ public class AgenteControl {
                 .fecAsignacion(agenteDTO.getFecAsignacion())
                 .codTipAgent(agenteDTO.getCodTipAgent())
                 .indOriginal(agenteDTO.getIndOriginal())
+                .numRedirecciones(agenteDTO.getNumRedirecciones())
+                .numDevoluciones(agenteDTO.getNumDevoluciones())
                 .tvsDatosContactoList(new ArrayList<>())
                 .build();
-    }
-
-    /**
-     * @param asignacion
-     * @return
-     */
-    private boolean puedeRedireccionar(AsignacionDTO asignacion) throws SystemException {
-        if (asignacion != null) {
-            int numRedirecciones = 0;
-            if (asignacion.getNumRedirecciones() != null)
-                numRedirecciones = Integer.parseInt(asignacion.getNumRedirecciones());
-            numRedirecciones++;
-            asignacionControl.actualizarNumRedirecciones(Integer.toString(numRedirecciones), asignacion.getIdeAsigUltimo());
-            return numRedirecciones <= numMaxRedirecciones;
-        } else return true;
     }
 }
