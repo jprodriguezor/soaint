b918a5e37d776dccb4da6c9cf3aed0196df391f4
Ernesto Tenorio <etenorio87@gmail.com>
2017-10-04 22:10:30 -0400

    ProdDoc: Montando las pantallas en Tabs

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/produccion-documental.component.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/produccion-documental.component.ts
@@ -2,45 +2,33 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
-    selector: 'produccion-documental',
-    templateUrl: './produccion-documental.component.html',
-    styleUrls: ['produccion-documental.component.css'],
+  selector: 'produccion-documental',
+  templateUrl: './produccion-documental.component.html',
+  styleUrls: ['produccion-documental.component.css'],
 })
 
 export class ProduccionDocumentalComponent implements OnInit{
 
-    constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(private activatedRoute: ActivatedRoute) {}
 
-    step : number = 1;
-    revisar: boolean = false;
-    aprobar: boolean = false;
-
-    next() : boolean {
-        if (this.step === 3) {
-
-            this.step = 1;
-            return true;
-        }
-
-        this.step++;
-
-        return false;
-    }
-
-    prev() : void {
-        this.step--;
-    }
+  revisar: boolean = false;
+  aprobar: boolean = false;
+  tabIndex = 0;
 
 
 
-    ngOnInit(): void {
+  updateTabIndex(event) {
+    this.tabIndex = event.index;
+  }
 
-        this.activatedRoute.params.subscribe((params: Params) => {
-            let action = params.hasOwnProperty('action')? params['action']:'';
-            this.revisar = (action == 'revisar');
-            this.aprobar = (action == 'aprobar');
-            console.log("Aprobar = "+this.aprobar+"; Revisar = "+this.revisar+";");
-        });
+  ngOnInit(): void {
 
-    }
+    this.activatedRoute.params.subscribe((params: Params) => {
+        let action = params.hasOwnProperty('action')? params['action']:'';
+        this.revisar = (action == 'revisar');
+        this.aprobar = (action == 'aprobar');
+        console.log("Aprobar = "+this.aprobar+"; Revisar = "+this.revisar+";");
+    });
+
+  }
 }
