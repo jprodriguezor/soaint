d762dea92a442d968ea01f6bc247734cfccb2a56
Ernesto Tenorio <etenorio87@gmail.com>
2017-10-18 19:38:18 -0400

    ProdDoc: Creando el cliente en el API Gateway

---
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/delegator/ProduccionDocumentalClient.java
@@ -0,0 +1,9 @@
+package co.com.foundation.sgd.apigateway.apis.delegator;
+
+import co.com.foundation.sgd.infrastructure.ApiDelegator;
+import lombok.extern.log4j.Log4j2;
+
+@ApiDelegator
+@Log4j2
+public class ProduccionDocumentalClient {
+}
