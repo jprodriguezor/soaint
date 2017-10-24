0edcbbf936d46536786b912301e98b76b32d5715
Ernesto Tenorio <etenorio87@gmail.com>
2017-10-24 16:17:37 -0400

    ProdDoc: Completando la tarea despues de procesar la proyeccion multiple

--- soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/delegator/ProduccionDocumentalClient.java
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/delegator/ProduccionDocumentalClient.java
@@ -5,7 +5,6 @@ import co.com.soaint.foundation.canonical.bpm.EntradaProcesoDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
@@ -46,7 +45,7 @@ public class ProduccionDocumentalClient {
             procesoClient.iniciar(nuevaEntrada);
         }
 
-        return Response.status(HttpStatus.OK.value()).entity(nuevaEntrada).build();
+        return procesoClient.completarTarea(entrada);
     }
 
 }
