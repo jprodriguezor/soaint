5a1df0e29d16cb90e96297c2009d565db0fb09e4
Leinier Alvarez Armas <leinieralvarez@gmail.com>
2017-11-15 08:39:27 -0500

    endpoints

--- soaint-sgd-ui/soaint-sgd-web/src/environments/environment.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/environments/environment.ts
@@ -2,8 +2,8 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-const host = 'http://192.168.3.242:28080/soaint-sgd-web-api-gateway/apis';
-const ecmHost = 'http://192.168.3.242:28080/ecm-integration-services/apis/ecm';
+const host = 'http://192.168.1.152:28080/soaint-sgd-web-api-gateway/apis';
+const ecmHost = 'http://192.168.1.152:28080/ecm-integration-services/apis/ecm';
 // const host = 'http://192.168.1.81:28080/soaint-sgd-web-api-gateway/apis';
 // const ecmHost = 'http://192.168.1.81:28080/ecm-integration-services/apis/ecm';
 // const host = 'http://192.168.99.100:8080/soaint-sgd-web-api-gateway/apis';
