8de7366971671db102587f99d710665d2a29e79c
Leinier Alvarez Armas <leinieralvarez@gmail.com>
2017-11-23 12:42:45 -0500

    Cargar tareas al cambiar la dependencia

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/workspace/workspace.component.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/workspace/workspace.component.ts
@@ -1,27 +1,43 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {TareaDTO} from 'app/domain/tareaDTO';
 import {State as RootState} from 'app/infrastructure/redux-store/redux-reducers';
 import {Store} from '@ngrx/store';
 import {getArrayData} from 'app/infrastructure/state-management/tareasDTO-state/tareasDTO-selectors';
 import {Sandbox as TaskDtoSandbox} from 'app/infrastructure/state-management/tareasDTO-state/tareasDTO-sandbox';
+import {getSelectedDependencyGroupFuncionario} from '../../../infrastructure/state-management/funcionarioDTO-state/funcionarioDTO-selectors';
+import {Subscription} from 'rxjs/Subscription';
+import {DependenciaDTO} from '../../../domain/dependenciaDTO';
 
 @Component({
   selector: 'app-workspace',
   templateUrl: './workspace.component.html'
 })
-export class WorkspaceComponent implements OnInit {
+export class WorkspaceComponent implements OnInit, OnDestroy {
 
   tasks$: Observable<TareaDTO[]>;
 
   selectedTask: any;
 
+  globalDependencySubcription: Subscription;
+
+  dependenciaSelected$: Observable<DependenciaDTO>;
+
   constructor(private _store: Store<RootState>, private _taskSandbox: TaskDtoSandbox) {
     this.tasks$ = this._store.select(getArrayData);
+
+    this.dependenciaSelected$ = this._store.select(getSelectedDependencyGroupFuncionario);
+    this.globalDependencySubcription = this.dependenciaSelected$.subscribe((result) => {
+      this._taskSandbox.loadDispatch();
+    });
   }
 
   ngOnInit() {
     this._taskSandbox.loadDispatch();
+  }
+
+  ngOnDestroy() {
+    this.globalDependencySubcription.unsubscribe();
   }
 
   iniciarTarea(task) {
