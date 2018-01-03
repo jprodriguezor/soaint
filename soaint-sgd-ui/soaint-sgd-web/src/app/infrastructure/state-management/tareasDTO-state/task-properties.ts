d33002d716f36cfbd24a2c6a46fef9d8a87c4cc9
diony <dionyuci@gmail.com>
2018-01-03 15:23:18 -0500

    Cambio de nombre en la tarea de digitalizacion inmediata

--- soaint-sgd-ui/soaint-sgd-web/src/app/infrastructure/state-management/tareasDTO-state/task-properties.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/infrastructure/state-management/tareasDTO-state/task-properties.ts
@@ -1,5 +1,6 @@
 export const TASK_RADICACION_ENTRADA = 'Radicar COF Entrada';
-export const TASK_DIGITALIZAR_DOCUMENTO = 'Digitalización inmediata';
+//export const TASK_DIGITALIZAR_DOCUMENTO = 'Digitalización inmediata';
+export const TASK_DIGITALIZAR_DOCUMENTO = 'Adjuntar documento';
 export const TASK_DOCUMENTOS_TRAMITES = 'Recibir y Gestionar Documentos';
 export const TASK_GENERAR_PLANILLA_ENTRADA = 'Generar Planilla de Entrada';
 export const TASK_CARGAR_PLANILLA_ENTRADA = 'Cargar Planilla de Entrada';
