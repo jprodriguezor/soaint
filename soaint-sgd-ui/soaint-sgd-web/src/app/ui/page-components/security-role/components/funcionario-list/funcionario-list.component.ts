35ed721fde932d8c45636777f749a4624a31a9b3
diony <dionyuci@gmail.com>
2018-01-09 17:00:27 -0500

    Busqueda de un funcionario para sus roles

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/security-role/components/funcionario-list/funcionario-list.component.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/security-role/components/funcionario-list/funcionario-list.component.ts
@@ -6,7 +6,7 @@ import {State as RootState, State} from 'app/infrastructure/redux-store/redux-re
 import {ApiBase} from '../../../../../infrastructure/api/api-base';
 import {environment} from '../../../../../../environments/environment';
 import {RolDTO} from '../../../../../domain/rolesDTO';
-
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'funcionario-role-list',
@@ -14,6 +14,9 @@ import {RolDTO} from '../../../../../domain/rolesDTO';
   styleUrls: ['./funcionario-list.component.css']
 })
 export class FuncionarioListComponent implements OnInit {
+
+  form: FormGroup;
+
 
   funcionarios: Observable<FuncionarioDTO[]> = new Observable<FuncionarioDTO[]>();
   funcionarioEdit: FuncionarioDTO;
@@ -26,14 +29,24 @@ export class FuncionarioListComponent implements OnInit {
   roles: Observable<RolDTO[]> = new Observable<RolDTO[]>();
   roleSelected: any;
 
-  constructor(private _store: Store<RootState>, private _api: ApiBase) {
+  constructor(private _store: Store<RootState>, private _api: ApiBase, private formBuilder: FormBuilder) {
+    this.initForm();
   }
 
   ngOnInit() {
     /*const payload = {cod_dependencia: '10401040'};
     const endpoint = `${environment.listarFuncionarios_endpoint}` + '/' + payload.cod_dependencia;
     this.funcionarios = this._api.list(endpoint).map(data => data.funcionarios);*/
-    this.searchFuncionarios(); this.loadAllRoles();
+    this.searchFuncionarios();
+    this.loadAllRoles();
+  }
+
+  initForm() {
+    this.form = this.formBuilder.group({
+      'nombre': [null],
+      'primerApellido': [null],
+      'segundoApellido': [null],
+    });
   }
 
   loadAllRoles() {
@@ -43,7 +56,11 @@ export class FuncionarioListComponent implements OnInit {
 
   searchFuncionarios() {
     const endpoint = `${environment.buscarFuncionarios_endpoint}`;
-    this.funcionarios = this._api.post(endpoint, this.funcionarioBusqueda);
+    this.funcionarioBusqueda.nombre = this.form.get('nombre').value;
+    this.funcionarioBusqueda.valApellido1 = this.form.get('primerApellido').value;
+    this.funcionarioBusqueda.valApellido2 = this.form.get('segundoApellido').value;
+
+    this.funcionarios = this._api.post(endpoint, this.funcionarioBusqueda).map( _data => _data.funcionarios);
   }
 
   editFuncionario(func: FuncionarioDTO): void {
