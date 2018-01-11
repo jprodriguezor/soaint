d8ad8342b7deac24c8b83ec44b7df32764356a53
Alien GR <aliengongora@gmail.com>
2018-01-11 16:19:25 -0500

    'Eliminando las comillas dobles.'

--- soaint-sgd-ui/soaint-sgd-web/src/app/app.routes.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/app.routes.ts
@@ -20,7 +20,7 @@ import {ProduccionDocumentalComponent} from './ui/page-components/produccion-doc
 import {ProduccionDocumentalMultipleComponent} from './ui/page-components/produccion-documental/produccion-documental-multiple.component';
 import {SeleccionarUnidadDocumentalComponent} from './ui/page-components/archivar-documento/seleccionar-unidad-documental/seleccionar-unidad-documental.component';
 import {SeleccionarDocumentosComponent} from './ui/page-components/archivar-documento/seleccionar-documentos/seleccionar-documentos.component';
-import {SecurityRoleComponent} from "./ui/page-components/security-role/security-role.component";
+import {SecurityRoleComponent} from './ui/page-components/security-role/security-role.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: ROUTES_PATH.dashboard, pathMatch: 'full'},
