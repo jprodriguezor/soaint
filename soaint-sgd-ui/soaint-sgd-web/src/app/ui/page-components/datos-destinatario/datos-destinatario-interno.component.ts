b14ad9c4bb8ce84190240112751a1ef1496959da
Ernesto Tenorio <etenorio87@gmail.com>
2018-01-16 20:35:00 -0500

    ProdDoc: Creando los componentes para los tipos de destinatarios interno y externo

---
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/datos-destinatario/datos-destinatario-interno.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'datos-destinatario-interno',
+    templateUrl: 'datos-destinatario-interno.component.html'
+})
+export class DatosDestinatarioInternoComponent implements OnInit, OnDestroy {
+
+
+
+    ngOnDestroy(): void {}
+
+    ngOnInit(): void {}
+}
