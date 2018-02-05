a521f42004ed2f03385ad0a5b2103bf2f17a3dea
NAUTILUS\Ernesto <etenorio87@gmail.com>
2018-02-05 16:29:15 -0500

    Tipos de datos de ProducciónDocumental

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/models/DocumentoDTO.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/models/DocumentoDTO.ts
@@ -1,8 +1,18 @@
 import {ConstanteDTO} from '../../../../domain/constanteDTO';
 
+export type TipoDocumento = 'PDF' | 'HTML';
+
 export interface AnexoDTO {
     soporte: string,
     tipo: ConstanteDTO,
     descripcion: string,
     file?: any,
+}
+
+export interface VersionDocumentoDTO {
+    tipo: TipoDocumento;
+    nombre: string;
+    size: number;
+    contenido?: string;
+
 }
--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/models/VersionDocumentoDTO.ts
+++
@@ -1,8 +0,0 @@
-export type TipoDocumento = 'PDF' | 'WORD' | 'EXCEL';
-
-export interface VersionDocumentoDTO {
-    tipo: TipoDocumento;
-    nombre: string;
-    size: number;
-    descripcion?: string;
-}
