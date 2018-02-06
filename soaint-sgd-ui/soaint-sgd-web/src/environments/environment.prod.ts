25045334d949e5c155e410af0807ebd67ee16751
diony <dionyuci@gmail.com>
2018-02-06 16:26:39 -0500

    add tipoComunicacionSalida_endpoint

--- soaint-sgd-ui/soaint-sgd-web/src/environments/environment.prod.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/environments/environment.prod.ts
@@ -16,6 +16,7 @@ export const environment = {
   tipoPlantilla_endpoint: `${host}/tipo-plantilla-gateway-api`,
   tipoDocumento_endpoint: `${host}/tipo-documento-gateway-api`,
   tipoComunicacion_endpoint: `${host}/tipo-comunicacion-gateway-api`,
+  tipoComunicacionSalida_endpoint: `${host}/tipo-comunicacion-salida-gateway-api`,
   tipoAnexos_endpoint: `${host}/tipo-anexos-gateway-api`,
   soporteAnexo_endpoint: `${host}/soporte-anexos-gateway-api`,
   sedeAdministrativa_endpoint: `${host}/sede-administrativa-gateway-api`,
