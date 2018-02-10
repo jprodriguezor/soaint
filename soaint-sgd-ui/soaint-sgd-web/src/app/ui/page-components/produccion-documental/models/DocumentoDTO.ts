355f1e16a8067536c100adae67d675ce8248f9d5
Ernesto Tenorio <etenorio87@gmail.com>
2018-02-09 22:43:24 -0500

    ProdDoc: Refactorizando los servicios

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/models/DocumentoDTO.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/models/DocumentoDTO.ts
@@ -15,31 +15,7 @@ export interface VersionDocumentoDTO {
     tipo: string,
     nombre: string,
     size: number,
-    contenido?: string,
-    idEcm?: string,
     version?: string,
+    contenido?: string,
     file?: Blob
-}
-
-export class VersionDocumento implements VersionDocumentoDTO{
-    id: string;
-    tipo: string;
-    nombre: string;
-    size: number;
-    contenido?: string;
-    idEcm?: string;
-    version?: string;
-    file?: Blob;
-
-    constructor (nombre?: string, contenido?: string, size?: number, type?: string, file?: Blob) {
-      this.id = 'none';
-      this.tipo = type || (file && file.type) || 'html';
-      this.size = size || (file && Math.ceil(file.size / 1024)) || 0;
-      this.nombre = nombre || (file && (<File>file).name) || '';
-      this.contenido = contenido || '';
-      this.file = file || null;
-      this.idEcm = null;
-
-      if (contenido && contenido.length > 0) {this.size = Math.ceil(this.contenido.length / 1024)}
-    }
 }
