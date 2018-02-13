0259b064ac5bc2e41e639c286bc73836ff734f0e
Ernesto Tenorio <etenorio87@gmail.com>
2018-02-13 18:38:24 -0500

    ProdDoc: Endpoint para obtrener los funcionarios por loginnames

--- soaint-sgd-ui/soaint-sgd-web/src/environments/environment.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/environments/environment.ts
@@ -100,7 +100,6 @@ export const environment = {
   pd_gestion_documental : {
     subirAnexo: `${ecmHost}/subirDocumentoRelacionECM`,
     obtenerAnexo: `${ecmHost}/obtenerDocumentosAdjuntosECM`,
-    //subirAnexo: `${host}/produccion-documental-gateway-api/adjuntar/documento`,
     subirDocumentoVersionado : `${ecmHost}/subirVersionarDocumentoGeneradoECM`,
     obtenerVersionDocumento : `${ecmHost}/descargarDocumentoVersionECM`,
     eliminarVersionDocumento : `${ecmHost}/eliminarDocumentoECM`,
