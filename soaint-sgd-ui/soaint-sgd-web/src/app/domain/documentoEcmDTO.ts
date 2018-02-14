0dfd37f6c22e7ace546e5448c6b556e0820f7d13
Ernesto Tenorio <etenorio87@gmail.com>
2018-02-14 10:24:53 -0500

    Agregando DTO del ECM

---
+++ soaint-sgd-ui/soaint-sgd-web/src/app/domain/documentoEcmDTO.ts
@@ -0,0 +1,15 @@
+export interface DocumentoEcmDTO {
+    idDocumento: string,
+    nroRadicado: string,
+    tipologiaDocumental: string,
+    nombreRemitente: string,
+    sede: string,
+    dependencia: string,
+    nombreDocumento: string,
+    idDocumentoPadre: string,
+    fechaCreacion: string,
+    tipoDocumento: string,
+    tamano: string,
+    tipoPadreAdjunto: string,
+    versionLabel: string
+}
