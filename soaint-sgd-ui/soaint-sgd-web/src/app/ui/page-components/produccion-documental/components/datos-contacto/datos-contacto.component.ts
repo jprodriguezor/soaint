bb0ea9d7a9b01433339c7a6068e6c58093db9981
Alien GR <aliengongora@gmail.com>
2018-02-17 14:53:26 -0500

    Digitalizar y Radicación de Salida

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/components/datos-contacto/datos-contacto.component.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/components/datos-contacto/datos-contacto.component.ts
@@ -26,9 +26,8 @@ export class PDDatosContactoComponent implements OnInit, OnDestroy {
   listaDestinatariosInternos: DestinatarioDTO[] = [];
   listaDestinatariosExternos: DestinatarioDTO[] = [];
 
-  destinatarioInterno: DestinatarioDTO;
-  destinatarioExterno: DestinatarioDTO;
-
+  destinatarioInterno: DestinatarioDTO = null;
+  destinatarioExterno: DestinatarioDTO = null;
 
   @ViewChild('datosRemitentes') datosRemitentes;
 
@@ -37,7 +36,7 @@ export class PDDatosContactoComponent implements OnInit, OnDestroy {
   canInsert = false;
   responderRemitente = false;
   hasNumberRadicado = false;
-  editable = false;
+  editable = true;
 
   destinatarioExternoDialogVisible = false;
   destinatarioInternoDialogVisible = false;
