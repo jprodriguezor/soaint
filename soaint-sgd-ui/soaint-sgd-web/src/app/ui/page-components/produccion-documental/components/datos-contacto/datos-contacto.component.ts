a99b38a1218830a290e67eb8b82ccc15ba85f95e
diony <dionyuci@gmail.com>
2018-02-17 19:07:45 -0500

    cambios

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/components/datos-contacto/datos-contacto.component.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/produccion-documental/components/datos-contacto/datos-contacto.component.ts
@@ -18,146 +18,141 @@ import {Sandbox as DependenciaSandbox} from 'app/infrastructure/state-management
 })
 
 export class PDDatosContactoComponent implements OnInit, OnDestroy {
-  form: FormGroup;
+  form:FormGroup;
 
-  tipoComunicacionSelected = 'EE';
+  subscription:Subscription;
 
-  subscription: Subscription;
+  validations:any = {};
 
-  validations: any = {};
+  listaDestinatariosInternos:DestinatarioDTO[] = [];
+  listaDestinatariosExternos:DestinatarioDTO[] = [];
 
-  listaDestinatariosInternos: DestinatarioDTO[] = [];
-  listaDestinatariosExternos: DestinatarioDTO[] = [];
+  destinatarioInterno:DestinatarioDTO = null;
+  destinatarioExterno:DestinatarioDTO = null;
 
-  destinatarioInterno: DestinatarioDTO = null;
-  destinatarioExterno: DestinatarioDTO = null;
-
-  @ViewChild('datosRemitentesExterno') datosRemitentesExterno;
-  @ViewChild('datosRemitentesInterno') datosRemitentesInterno;
-
-  @Input() taskData: TareaDTO;
-
-  canInsert = false;
   responderRemitente = false;
   hasNumberRadicado = false;
   editable = true;
   defaultDestinatarioTipoComunicacion = "";
-
-  valueRemitente: any;
+  hasDestinatarioPrincipal = false;
 
   destinatarioExternoDialogVisible = false;
   destinatarioInternoDialogVisible = false;
 
+  @ViewChild('datosRemitentesExterno') datosRemitentesExterno;
+  @ViewChild('datosRemitentesInterno') datosRemitentesInterno;
 
-  constructor(private formBuilder: FormBuilder,
-              private _changeDetectorRef: ChangeDetectorRef,
-              private _produccionDocumentalApi: ProduccionDocumentalApiService,
-              private pdMessageService: PdMessageService,
-              private _dependenciaSandbox: DependenciaSandbox) {
+  @Input() taskData:TareaDTO;
+
+  constructor(private formBuilder:FormBuilder,
+              private _changeDetectorRef:ChangeDetectorRef,
+              private _produccionDocumentalApi:ProduccionDocumentalApiService,
+              private pdMessageService:PdMessageService,
+              private _dependenciaSandbox:DependenciaSandbox) {
 
     /*this.subscription = this.pdMessageService.getMessage().subscribe(tipoComunicacion => {
 
-      this.tipoComunicacionSelected = tipoComunicacion;
-      this.responseToRem = false;
-      this.form.get('responderRemitente').setValue(false);
+     this.tipoComunicacionSelected = tipoComunicacion;
+     this.responseToRem = false;
+     this.form.get('responderRemitente').setValue(false);
 
-      if(this.taskData.variables.numeroRadicado){
+     if(this.taskData.variables.numeroRadicado){
 
-        this._produccionDocumentalApi.obtenerContactosDestinatarioExterno({
-          nroRadicado: this.taskData.variables.numeroRadicado
-        }).subscribe( contacto => {
-          console.log(contacto);
+     this._produccionDocumentalApi.obtenerContactosDestinatarioExterno({
+     nroRadicado: this.taskData.variables.numeroRadicado
+     }).subscribe( contacto => {
+     console.log(contacto);
 
-          this.hasNumberRadicado = false;
+     this.hasNumberRadicado = false;
 
-          if(contacto.codTipoRemite == "EXT" && this.tipoComunicacionSelected && this.tipoComunicacionSelected.codigo === 'SE'){
-            this.hasNumberRadicado = true;
-          }else if(contacto.codTipoRemite == "INT" && this.tipoComunicacionSelected && this.tipoComunicacionSelected.codigo === 'SI'){
-            this.hasNumberRadicado = true;
-          }
+     if(contacto.codTipoRemite == "EXT" && this.tipoComunicacionSelected && this.tipoComunicacionSelected.codigo === 'SE'){
+     this.hasNumberRadicado = true;
+     }else if(contacto.codTipoRemite == "INT" && this.tipoComunicacionSelected && this.tipoComunicacionSelected.codigo === 'SI'){
+     this.hasNumberRadicado = true;
+     }
 
-          if(this.tipoComunicacionSelected && this.tipoComunicacionSelected.codigo === 'SE'){
-            this.remitenteExterno = contacto;
-            this.datosDestinatarioExterno.getDestinatarioDefault(this.remitenteExterno);
-          }else if(this.tipoComunicacionSelected && this.tipoComunicacionSelected.codigo === 'SI'){
-            //this.listaDestinatariosInternos.push(contacto);
-            console.log("entro por contacto " + contacto );
+     if(this.tipoComunicacionSelected && this.tipoComunicacionSelected.codigo === 'SE'){
+     this.remitenteExterno = contacto;
+     this.datosDestinatarioExterno.getDestinatarioDefault(this.remitenteExterno);
+     }else if(this.tipoComunicacionSelected && this.tipoComunicacionSelected.codigo === 'SI'){
+     //this.listaDestinatariosInternos.push(contacto);
+     console.log("entro por contacto " + contacto );
 
-            this._dependenciaSandbox.loadDependencies({}).subscribe((results) => {
-              console.log(results.dependencias.find((element) => element.codigo === contacto.codDependencia));
-              console.log(results.dependencias.find((element) => element.codSede === contacto.codSede));
-              this.defaultDestinatarioInterno = {
-                sede: results.dependencias.find((element) => element.codigo === contacto.codDependencia),
-                depedencia: results.dependencias.find((element) => element.codSede === contacto.codSede)
-              }
-            });
+     this._dependenciaSandbox.loadDependencies({}).subscribe((results) => {
+     console.log(results.dependencias.find((element) => element.codigo === contacto.codDependencia));
+     console.log(results.dependencias.find((element) => element.codSede === contacto.codSede));
+     this.defaultDestinatarioInterno = {
+     sede: results.dependencias.find((element) => element.codigo === contacto.codDependencia),
+     depedencia: results.dependencias.find((element) => element.codSede === contacto.codSede)
+     }
+     });
 
-            this.defaultDestinatarioInterno = contacto;
-          }
+     this.defaultDestinatarioInterno = contacto;
+     }
 
-        });
-      }
-    });*/
+     });
+     }
+     });*/
 
     this.initForm();
   }
 
-  ngOnInit(): void {
+  ngOnInit():void {
 
     console.log("Tarea de entrada");
     console.log(this.taskData);
 
-    if(this.taskData.variables.numeroRadicado){
+    if (this.taskData.variables.numeroRadicado) {
       this.hasNumberRadicado = true;
     }
 
     this.form.get('responderRemitente').valueChanges.subscribe(responderRemitente => {
       this.responderRemitente = responderRemitente;
 
-      if(responderRemitente) {
+      if (responderRemitente) {
 
         if (this.taskData.variables.numeroRadicado) {
           this._produccionDocumentalApi.obtenerContactosDestinatarioExterno({
             nroRadicado: this.taskData.variables.numeroRadicado
           }).subscribe(agente => {
 
-            console.log("Objeto que viene del backen ",agente);
+            console.log("Objeto que viene del backen ", agente);
 
-            if(agente){
+            if (agente) {
 
               this.defaultDestinatarioTipoComunicacion = agente.codTipoRemite;
 
-               let tempDestinatario = <DestinatarioDTO> {};
+              let tempDestinatario = <DestinatarioDTO> {};
 
-                tempDestinatario.interno = false;
-                tempDestinatario.tipoDestinatario = (agente.indOriginal) ? agente.indOriginal : null;
-                tempDestinatario.tipoPersona = (agente.codTipoPers) ? agente.codTipoPers : null;
-                tempDestinatario.nombre= (agente.nombre) ? agente.nombre : "";
-                tempDestinatario.tipoDocumento=(agente.codTipDocIdent) ? agente.codTipDocIdent : null;
-                tempDestinatario.nit= (agente.nit) ? agente.nit : "";
-                tempDestinatario.actuaCalidad= (agente.codEnCalidad) ? agente.codEnCalidad : null;
-                tempDestinatario.actuaCalidadNombre= (agente.codEnCalidad) ? agente.codEnCalidad : "";
-                tempDestinatario.sede = (agente.codSede) ? agente.codSede : null;
-                tempDestinatario.dependencia = (agente.codDependencia) ? agente.codDependencia : null;
-                tempDestinatario.funcionario = null;
-                tempDestinatario.email = "";
-                tempDestinatario.mobile= "";
-                tempDestinatario.phone= "";
-                tempDestinatario.pais = null;
-                tempDestinatario.departamento= null;
-                tempDestinatario.municipio = null;
-                tempDestinatario.datosContactoList=(agente.datosContactoList) ? agente.datosContactoList : null;
-                tempDestinatario.principal = false;
+              tempDestinatario.interno = false;
+              tempDestinatario.tipoDestinatario = (agente.indOriginal) ? agente.indOriginal : null;
+              tempDestinatario.tipoPersona = (agente.codTipoPers) ? agente.codTipoPers : null;
+              tempDestinatario.nombre = (agente.nombre) ? agente.nombre : "";
+              tempDestinatario.tipoDocumento = (agente.codTipDocIdent) ? agente.codTipDocIdent : null;
+              tempDestinatario.nit = (agente.nit) ? agente.nit : "";
+              tempDestinatario.actuaCalidad = (agente.codEnCalidad) ? agente.codEnCalidad : null;
+              tempDestinatario.actuaCalidadNombre = (agente.codEnCalidad) ? agente.codEnCalidad : "";
+              tempDestinatario.sede = (agente.codSede) ? agente.codSede : null;
+              tempDestinatario.dependencia = (agente.codDependencia) ? agente.codDependencia : null;
+              tempDestinatario.funcionario = null;
+              tempDestinatario.email = "";
+              tempDestinatario.mobile = "";
+              tempDestinatario.phone = "";
+              tempDestinatario.pais = null;
+              tempDestinatario.departamento = null;
+              tempDestinatario.municipio = null;
+              tempDestinatario.datosContactoList = (agente.datosContactoList) ? agente.datosContactoList : null;
+              tempDestinatario.principal = false;
 
-              if(agente.codTipoRemite == "EXT"){
+              if (agente.codTipoRemite == "EXT") {
 
                 tempDestinatario.interno = false;
                 this.destinatarioExterno = tempDestinatario;
                 this.destinatarioExternoDialogVisible = true;
 
-              }else if(agente.codTipoRemite == "INT"){
+              } else if (agente.codTipoRemite == "INT") {
 
-                tempDestinatario.interno= true;
+                tempDestinatario.interno = true;
                 this.destinatarioInterno = tempDestinatario;
                 this.destinatarioInternoDialogVisible = true;
               }
@@ -167,20 +162,20 @@ export class PDDatosContactoComponent implements OnInit, OnDestroy {
           });
         }
 
-      }else{
+      } else {
 
         console.log("le doy al checke ", this.destinatarioExterno);
 
-        if(this.defaultDestinatarioTipoComunicacion == "EXT"){
+        if (this.defaultDestinatarioTipoComunicacion == "EXT") {
 
-          const index: number = this.listaDestinatariosExternos.indexOf(this.destinatarioExterno);
+          const index:number = this.listaDestinatariosExternos.indexOf(this.destinatarioExterno);
           if (index !== -1) {
             this.listaDestinatariosExternos.splice(index, 1);
           }
 
-        }else if(this.defaultDestinatarioTipoComunicacion == "INT"){
+        } else if (this.defaultDestinatarioTipoComunicacion == "INT") {
 
-          const index: number = this.listaDestinatariosInternos.indexOf(this.destinatarioInterno);
+          const index:number = this.listaDestinatariosInternos.indexOf(this.destinatarioInterno);
           if (index !== -1) {
             this.listaDestinatariosInternos.splice(index, 1);
           }
@@ -194,19 +189,19 @@ export class PDDatosContactoComponent implements OnInit, OnDestroy {
   //guardar para la base de datos
   /*updateStatus(currentStatus: StatusDTO) {
 
-    console.log('entro en el updateStatus');
-    console.log(currentStatus);
+   console.log('entro en el updateStatus');
+   console.log(currentStatus);
 
-    this.form.get('responderRemitente').setValue(currentStatus.datosContacto.responderRemitente);
-    this.form.get('distribucion').setValue(currentStatus.datosContacto.distribucion);
+   this.form.get('responderRemitente').setValue(currentStatus.datosContacto.responderRemitente);
+   this.form.get('distribucion').setValue(currentStatus.datosContacto.distribucion);
 
-    if (currentStatus.datosGenerales.tipoComunicacion.codigo === 'SI') {
-      this.listaDestinatariosInternos = [...currentStatus.datosContacto.listaDestinatarios];
-    } else if (currentStatus.datosGenerales.tipoComunicacion.codigo === 'SE') {
-      this.remitenteExterno = currentStatus.datosContacto.remitenteExterno;
-    }
-    this.refreshView();
-  }*/
+   if (currentStatus.datosGenerales.tipoComunicacion.codigo === 'SI') {
+   this.listaDestinatariosInternos = [...currentStatus.datosContacto.listaDestinatarios];
+   } else if (currentStatus.datosGenerales.tipoComunicacion.codigo === 'SE') {
+   this.remitenteExterno = currentStatus.datosContacto.remitenteExterno;
+   }
+   this.refreshView();
+   }*/
 
   initForm() {
     this.form = this.formBuilder.group({
@@ -216,45 +211,73 @@ export class PDDatosContactoComponent implements OnInit, OnDestroy {
     });
   }
 
-  showAddDestinatarioExternoPopup(){
-    this.tipoComunicacionSelected = "EE";
+  showAddDestinatarioExternoPopup() {
     this.datosRemitentesExterno.initFormByDestinatario(this.destinatarioExterno);
-
     this.destinatarioExternoDialogVisible = true;
   }
 
-  showAddDestinatarioInternoPopup(){
-    this.tipoComunicacionSelected = "EI";
+  showAddDestinatarioInternoPopup() {
     this.datosRemitentesInterno.initFormByDestinatario(this.destinatarioInterno);
-
     this.destinatarioInternoDialogVisible = true;
   }
 
-  hideAddDestinatarioExternoPopup(){
+  hideAddDestinatarioExternoPopup() {
     this.destinatarioExternoDialogVisible = false;
   }
 
-  hideAddDestinatarioInternoPopup(){
+  hideAddDestinatarioInternoPopup() {
     this.destinatarioInternoDialogVisible = false;
   }
 
-  addDestinatario(newDestinatario){
+  addDestinatario(newDestinatario) {
 
     console.log(newDestinatario);
 
-    if(newDestinatario.interno){
-      this.listaDestinatariosInternos = [newDestinatario, ...this.listaDestinatariosInternos];
-    }else{
-      this.listaDestinatariosExternos = [newDestinatario, ...this.listaDestinatariosExternos];
+    if (newDestinatario) {
+
+      if (!this.hasDestinatarioPrincipal) {
+        this.hasDestinatarioPrincipal = (newDestinatario.principal) ? true : false;
+      }
+
+      if (newDestinatario.interno) {
+        this.listaDestinatariosInternos = [newDestinatario, ...this.listaDestinatariosInternos];
+      } else {
+        this.listaDestinatariosExternos = [newDestinatario, ...this.listaDestinatariosExternos];
+      }
     }
-
-    //this.valueRemitente = this.datosRemitentes.form.value;
-    //this.listaDestinatariosExternos.push(this.destinatarioExterno);
-
-    console.log("Lo que trae el form ",this.listaDestinatariosExternos);
   }
 
-  findDestinatarioPrincipal(listDestinatarios){}
+
+  editDestinatario(index, op) {
+
+    this.datosRemitentesExterno.initFormByDestinatario(this.listaDestinatariosExternos[index]);
+
+    if (op == "EXT") {
+      this.destinatarioExternoDialogVisible = true;
+    } else if (op == "INT") {
+      this.destinatarioInternoDialogVisible = true;
+    }
+  }
+
+  deleteDestinatario(index, op) {
+
+    if (op == 'EXT') {
+      const radref = [...this.listaDestinatariosExternos];
+
+      this.hasDestinatarioPrincipal = (this.listaDestinatariosExternos[index].principal) ? false : true;
+
+      radref.splice(index, 1);
+      this.listaDestinatariosExternos = radref;
+
+    } else if (op == 'INT') {
+
+      this.hasDestinatarioPrincipal = (this.listaDestinatariosInternos[index].principal) ? false : true;
+
+      const radref = [...this.listaDestinatariosInternos];
+      radref.splice(index, 1);
+      this.listaDestinatariosInternos = radref;
+    }
+  }
 
   ngOnDestroy() {
     //this.subscription.unsubscribe();
