aa0a82f0c5b03aecacca60c9f380faadf667f45d
Dasiel Otero Dartayet <dasiel@gmail.com>
2018-02-22 12:00:08 -0500

    ADD DTO Unidad Documental

---
+++ soaint-sgd-enterprise-artifacts/canonical-api-contract/src/main/java/co/com/soaint/foundation/canonical/ecm/UnidadDocumentalDTO.java
@@ -0,0 +1,36 @@
+package co.com.soaint.foundation.canonical.ecm;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ * Soaint Generic Artifact
+ * Created:22-Feb-2018
+ * Author: dotero
+ * Type: JAVA class Artifact
+ * Purpose: DTO - Model Artifact
+ * ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(builderMethodName = "newInstance")
+@XmlRootElement(namespace = "http://soaint.com/domain-artifacts/ecm/organigrama/1.0.0")
+public class UnidadDocumentalDTO {
+
+    private String ideUnidadDocumental;
+    private String codigoUnidadDocumental;
+    private String nombreUnidadDocumental;
+    private String descriptor1;
+    private String descriptor2;
+    private String codigoSerie;
+    private String nombreSerie;
+    private String codigoSubSerie;
+    private String nombreSubSerie;
+}
