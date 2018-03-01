e013442d66d89b06507cc4cdfa142e4d017cce70
dagmar <dfrod2015@gmail.com>
2018-03-01 14:17:38 -0500

    1/3/2018 14:17

--- soaint-sgd-ui/soaint-sgd-web/src/environments/environment.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/environments/environment.ts
@@ -3,7 +3,6 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-
 const host = 'http://192.168.1.81:28080/soaint-sgd-web-api-gateway/apis';
 const ecmHost = 'http://192.168.1.81:28080/ecm-integration-services/apis/ecm';
 
