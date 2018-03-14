ad2a1c82060017bf31703a1e63c41ab4532bfd94
Lissett <lissettdm@outlook.es>
2018-03-14 15:57:02 -0400

    corregir radicacion

---
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/corregir-radicacion/corregir-radication.state.service.ts
@@ -0,0 +1,116 @@
+import {Injectable} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import {Store} from '@ngrx/store';
+import {State} from 'app/infrastructure/redux-store/redux-reducers';
+import {Sandbox as ConstanteSandbox} from 'app/infrastructure/state-management/constanteDTO-state/constanteDTO-sandbox';
+import {Subscription} from 'rxjs/Subscription';
+import {Observable} from 'rxjs/Observable';
+import {TareaDTO} from '../../../domain/tareaDTO';
+import {Sandbox as RadicarComunicacionesSandBox} from 'app/infrastructure/state-management/radicarComunicaciones-state/radicarComunicaciones-sandbox';
+import {Sandbox as TaskSandBox} from 'app/infrastructure/state-management/tareasDTO-state/tareasDTO-sandbox';
+import {getActiveTask} from '../../../infrastructure/state-management/tareasDTO-state/tareasDTO-selectors';
+import {Sandbox as AsiganacionDTOSandbox} from '../../../infrastructure/state-management/asignacionDTO-state/asignacionDTO-sandbox';
+import {ComunicacionOficialDTO} from '../../../domain/comunicacionOficialDTO';
+import {RadicacionEntradaDTV} from '../../../shared/data-transformers/radicacionEntradaDTV';
+import {FuncionarioDTO} from '../../../domain/funcionarioDTO';
+import {getArrayData as getFuncionarioArrayData, getAuthenticatedFuncionario, getSelectedDependencyGroupFuncionario} from '../../../infrastructure/state-management/funcionarioDTO-state/funcionarioDTO-selectors';
+import {DependenciaDTO} from '../../../domain/dependenciaDTO';
+import {ROUTES_PATH} from '../../../app.route-names';
+import {go} from '@ngrx/router-store';
+import {Sandbox as DependenciaSandbox} from '../../../infrastructure/state-management/dependenciaGrupoDTO-state/dependenciaGrupoDTO-sandbox';
+import {Sandbox as CominicacionOficialSandbox} from '../../../infrastructure/state-management/comunicacionOficial-state/comunicacionOficialDTO-sandbox';
+
+
+@Injectable()
+export class CorregirRadicacionStateService {
+
+task: TareaDTO;
+activeTaskUnsubscriber: Subscription;
+
+radicacion: ComunicacionOficialDTO;
+radicacionEntradaDTV: any;
+comunicacion: ComunicacionOficialDTO = {};
+
+destinatario: any;
+generales: any;
+remitente: any;
+descripcionAnexos: any;
+radicadosReferidos: any;
+agentesDestinatario: any;
+
+  constructor(
+      private _store: Store<State>,
+      private _sandbox: RadicarComunicacionesSandBox,
+      private _asiganacionSandbox: AsiganacionDTOSandbox,
+      private _comunicacionOficialApi: CominicacionOficialSandbox,
+      private _taskSandBox: TaskSandBox,
+      private formBuilder: FormBuilder
+    ) {
+
+  }
+
+  Init() {
+    this.activeTaskUnsubscriber = this._store.select(getActiveTask).subscribe(activeTask => {
+        this.task = activeTask;
+        this.restore();
+    });
+  }
+
+  abort() {
+    this._taskSandBox.abortTaskDispatch({
+      idProceso: this.task.idProceso,
+      idDespliegue: this.task.idDespliegue,
+      instanciaProceso: this.task.idInstanciaProceso
+    });
+  }
+
+  save(): Observable<any> {
+    const payload: any = {
+      destinatario: this.destinatario,
+      generales: this.generales,
+      remitente: this.remitente,
+      descripcionAnexos: this.descripcionAnexos,
+      radicadosReferidos: this.radicadosReferidos,
+      agentesDestinatario: this.agentesDestinatario
+    };
+
+    if (this.remitente.datosContactos) {
+      payload.datosContactos = this.remitente.datosContactos.contacts;
+    }
+
+    const tareaDto: any = {
+      idTareaProceso: this.task.idTarea,
+      idInstanciaProceso: this.task.idInstanciaProceso,
+      payload: payload
+    };
+
+    return this._sandbox.quickSave(tareaDto);
+  }
+
+  restore() {
+    if (this.task) {
+      this._asiganacionSandbox.obtenerComunicacionPorNroRadicado(this.task.variables.numeroRadicado).subscribe((result) => {
+        this.comunicacion = result;
+        console.log(this.comunicacion);
+
+        this.radicacionEntradaDTV = new RadicacionEntradaDTV(this.comunicacion);
+        console.log(this.radicacionEntradaDTV);
+        this.remitente.form.patchValue(this.radicacionEntradaDTV.getDatosRemitente());
+        this.generales.form.patchValue(this.radicacionEntradaDTV.getRadicacionEntradaFormData());
+      });
+    }
+  }
+  actualizarComunicacion() {
+    this._comunicacionOficialApi.actualizarComunicacion(this.comunicacion);
+    this._taskSandBox.completeTaskDispatch({
+      idProceso: this.task.idProceso,
+      idDespliegue: this.task.idDespliegue,
+      idTarea: this.task.idTarea,
+      parametros: {
+      }
+    });
+    this._store.dispatch(go(['/' + ROUTES_PATH.workspace]));
+  }
+
+}
