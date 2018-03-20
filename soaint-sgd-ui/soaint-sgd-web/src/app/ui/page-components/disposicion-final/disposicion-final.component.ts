ef87894bc527f4fe172089a99fd3788d780c84ed
Ernesto Tenorio <etenorio87@gmail.com>
2018-03-20 12:37:56 -0400

    Mostrando las sedes, dependencias, series y subseries en Disposicion Final

--- soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/disposicion-final/disposicion-final.component.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/ui/page-components/disposicion-final/disposicion-final.component.ts
@@ -1,8 +1,19 @@
 import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { State } from '../../../infrastructure/redux-store/redux-reducers';
 import { TareaDTO } from 'app/domain/tareaDTO';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Rx';
 import { ConstanteDTO } from 'app/domain/constanteDTO';
+import {LoadAction as SedeAdministrativaLoadAction} from '../../../infrastructure/state-management/sedeAdministrativaDTO-state/sedeAdministrativaDTO-actions';
+import {getArrayData as dependenciaGrupoArrayData} from '../../../infrastructure/state-management/dependenciaGrupoDTO-state/dependenciaGrupoDTO-selectors';
+import {getArrayData as sedeAdministrativaArrayData} from '../../../infrastructure/state-management/sedeAdministrativaDTO-state/sedeAdministrativaDTO-selectors';
+import {Sandbox as DependenciaGrupoSandbox} from '../../../infrastructure/state-management/dependenciaGrupoDTO-state/dependenciaGrupoDTO-sandbox';
+import { SerieDTO } from '../../../domain/serieDTO';
+import { SubserieDTO } from 'app/domain/subserieDTO';
+import { SerieService } from '../../../infrastructure/api/serie.service';
+
+
 
 @Component({
   selector: 'disposicion-final',
@@ -20,18 +31,31 @@ export class DisposicionFinalComponent implements OnInit, OnDestroy {
     tiposDisposicionFinal$: Observable<ConstanteDTO[]>;
     sedes$: Observable<ConstanteDTO[]>;
     dependencias$: Observable<ConstanteDTO[]>;
-    series$: Observable<ConstanteDTO[]>;
-    subseries$: Observable<ConstanteDTO[]>;
+    series$: Observable<SerieDTO[]>;
+    subseries$: Observable<SubserieDTO[]>;
 
     listaDisposiciones: any[];
     selectedItemsListaDisposiciones: any[];
 
-    constructor(
+    constructor(private _store: Store<State>,
               private formBuilder: FormBuilder,
+              private _dependenciaGrupoSandbox: DependenciaGrupoSandbox,
+              private _serieSubserieApi: SerieService,
               private _changeDetectorRef: ChangeDetectorRef) {
 
-    this.initForm();
-  }
+      this.initForm();
+      this.listenForChanges();
+
+      Observable.combineLatest(
+            this.formSearch.get('sede').valueChanges,
+            this.formSearch.get('dependencia').valueChanges,
+            this.formSearch.get('serie').valueChanges,
+            this.formSearch.get('subserie').valueChanges
+      ).filter(([sede, dependencia, serie, subserie]) => sede && dependencia && serie && subserie)
+      .zip(([sede, dependencia, serie, subserie]) => {
+          return {sede: sede, dependencia: dependencia, serie: serie, subserie: subserie}
+      });
+    }
 
     initForm() {
       this.formSearch = this.formBuilder.group({
@@ -47,9 +71,40 @@ export class DisposicionFinalComponent implements OnInit, OnDestroy {
       });
     }
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.sedes$ = this._store.select(sedeAdministrativaArrayData);
+        this.dependencias$ = this._store.select(dependenciaGrupoArrayData);
+        this._store.dispatch(new SedeAdministrativaLoadAction());
+    }
 
     ngOnDestroy() {}
+
+    listenForChanges() {
+        this.formSearch.get('sede').valueChanges.subscribe((value: ConstanteDTO) => {
+            if (value) {
+                this.formSearch.get('dependencia').reset();
+                this.formSearch.get('serie').reset();
+                this.formSearch.get('subserie').reset();
+                this._dependenciaGrupoSandbox.loadDispatch({codigo: value.id});
+                // this.series$ = this._serieSubserieApi.ListarSerieSubserie({idOrgOfc: codDependencia});
+            }
+        });
+
+        this.formSearch.get('dependencia').valueChanges.subscribe((value: ConstanteDTO) => {
+            if (value) {
+                this.formSearch.get('serie').reset();
+                this.formSearch.get('subserie').reset();
+                this.series$ = this._serieSubserieApi.getSeriePorDependencia(value.codigo);
+            }
+        });
+
+        this.formSearch.get('serie').valueChanges.subscribe((value) => {
+            if (value) {
+                this.formSearch.get('subserie').reset();
+                this.subseries$ = this._serieSubserieApi.getSubseriePorDependenciaSerie(this.formSearch.get('dependencia').value.codigo, value);
+            }
+        });
+    }
 
     transponer() {
         this.stacked = !this.stacked;
