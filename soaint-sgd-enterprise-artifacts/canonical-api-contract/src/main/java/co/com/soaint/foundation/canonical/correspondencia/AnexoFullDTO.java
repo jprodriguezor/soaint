e4a48aa9cf74b6a192b84febcc6c526019c72941
Giselle Yanet Prieto Ruiz <giselle.designe@gmail.com>
2018-03-21 16:56:26 -0700

    ADD:Contrato de obtener anexos por nroRadicado

---
+++ soaint-sgd-enterprise-artifacts/canonical-api-contract/src/main/java/co/com/soaint/foundation/canonical/correspondencia/AnexoFullDTO.java
@@ -0,0 +1,36 @@
+package co.com.soaint.foundation.canonical.correspondencia;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+import java.math.BigInteger;
+
+/**
+ * ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ * Soaint Generic Artifact
+ * Created:2-Jun-2017
+ * Author: esanchez
+ * Type: JAVA class Artifact
+ * Purpose: DTO - Model Artifact
+ * ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder(builderMethodName = "newInstance")
+@XmlRootElement(namespace = "http://soaint.com/domain-artifacts/cor-anexo/1.0.0")
+public class AnexoFullDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private BigInteger ideAnexo;
+    private String codAnexo;
+    private String descTipoAnexo;
+    private String descripcion;
+    private String codTipoSoporte;
+    private String descTipoSoporte;
+}
