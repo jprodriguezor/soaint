ad40f443b913abff1734c1b46e7d3fef7c85e30e
ytangv <ytangveitia@gmail.com>
2018-05-03 15:21:39 -0400

    Merge branch 'develop' of https://gitlab.com/jprodriguez1/Soaint-SGD into test_correspondencia
    Conflicts:
    	soaint-sgd-enterprise-services/ecm-integration-services/src/test/java/co/com/soaint/ecm/business/boundary/documentmanager/ContentControlAlfrescoTest.java

--- soaint-sgd-enterprise-services/correspondencia-business-services/src/test/java/co/com/soaint/correspondencia/FuncionariosTest.java
+++ soaint-sgd-enterprise-services/correspondencia-business-services/src/test/java/co/com/soaint/correspondencia/FuncionariosTest.java
@@ -1,35 +1,79 @@
 package co.com.soaint.correspondencia;
 
+
 import co.com.soaint.foundation.canonical.correspondencia.FuncionarioDTO;
-import co.com.soaint.foundation.framework.exceptions.SystemException;
+import co.com.soaint.correspondencia.domain.entity.Funcionarios;
+
+
 import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.math.BigInteger;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
- * Created by gyanet on 04/04/2018.
+ * Created by yleon on 04/04/2018.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(
         locations = {"/META-INF/core-config.xml"}
 )
 @Log4j2
-public class FuncionariosTest extends  JPAHibernateTest {
+public class FuncionariosTest extends JPAHibernateContextTest {
 
     @Test
-    public void test_guardarEstadoTarea_success() throws SystemException {
-
+    public void test_FuncionariosfindAll_success() {
         //given
+        String namedQuery = "Funcionarios.findAll";
 
         //when
-//        tareaControl.guardarEstadoTarea(tareaDTO);
+        List<Funcionarios> funcionarios = createNamedQuery(namedQuery, Funcionarios.class)
+                .getResultList();
+
         //then
+        assertNotNull(funcionarios);
+        assertTrue(funcionarios.size()>0);
+    }
+    @Test
+    public void test_FuncionariosfindByLoginNameAndEstado_success() {
+        //given
+        String namedQuery = "Funcionarios.findByLoginNameAndEstado";
+        String loginname="LOGIN_NAME";
+        String estado="ACTIVO";
+
+        //when
+        FuncionarioDTO funcionarios = createNamedQuery(namedQuery, FuncionarioDTO.class)
+                .setParameter("LOGIN_NAME",loginname)
+                .setParameter("ESTADO",estado)
+                .getSingleResult();
+
+        //then
+        assertNotNull(funcionarios);
+        assertEquals(funcionarios.getLoginName(),loginname);
 
     }
+    @Test
+    public void test_FuncionariosfindByLoginNamList_success() {
+        //given
+        String namedQuery = "Funcionarios.findByLoginNamList";
+        String loginname="LOGIN_NAME";
+
+
+        //when
+        List<FuncionarioDTO> funcionarios = createNamedQuery(namedQuery, FuncionarioDTO.class)
+                .setParameter("LOGIN_NAMES",loginname)
+                .getResultList();
+
+        //then
+        assertNotNull(funcionarios);
+        assertTrue(funcionarios.size()>0);
+
+    }
+
 }
