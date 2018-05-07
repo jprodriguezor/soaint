5c09cbef6941a7e9146d25a254e9caa41f320c63
Giselle Yanet <giselle.designe@gmail.com>
2018-05-07 14:19:31 -0500

    Fix Obtener Solicitud todos los campos a null

--- soaint-sgd-enterprise-services/correspondencia-business-services/src/main/java/co/com/soaint/correspondencia/domain/entity/TvsSolicitudUnidadDocumental.java
+++ soaint-sgd-enterprise-services/correspondencia-business-services/src/main/java/co/com/soaint/correspondencia/domain/entity/TvsSolicitudUnidadDocumental.java
@@ -32,7 +32,9 @@ import java.util.List;
             "(t.ideSolicitud, t.id, t.idConstante, t.fecHora, t.nombreUD, t.descriptor1, t.descriptor2, t.nro, t.codSerie, t.codSubserie, t.codSede,"+
             " t.codDependencia, t.idSolicitante, t.estado, t.accion, t.observaciones)" +
             "FROM TvsSolicitudUnidadDocumental t " +
-            "WHERE t.codDependencia = :COD_DEP AND t.codSede = :COD_SEDE AND (:FECHA_INI IS NULL OR (t.fecHora >= :FECHA_INI AND t.fecHora <= :FECHA_FIN))"),
+            "WHERE (:COD_DEP IS NULL OR t.codDependencia = :COD_DEP) " +
+            "AND (:COD_SEDE IS NULL OR t.codSede = :COD_SEDE) " +
+            "AND ((:FECHA_INI IS NULL OR t.fecHora >= :FECHA_INI) AND (:FECHA_FIN IS NULL OR t.fecHora <= :FECHA_FIN))"),
     @NamedQuery(name = "TvsSolicitudUM.actualizarSolicitudUnidadDocumental", query = "UPDATE TvsSolicitudUnidadDocumental t " +
             "SET  t.id = :ID, t.idConstante = :ID_CONST , t.fecHora = :FECH, t.nombreUD = :NOMBREUD, t.descriptor1 = :DESCP1, "+
             "t.descriptor2 = :DESCP2, t.nro = :NRO, t.codSerie = :COD_SER, t.codSubserie = :COD_SUBS, t.codSede = :COD_SED, t.codDependencia = :COD_DEP, "+
