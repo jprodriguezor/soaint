40d1f4980e6424b56abcc3336c39f689a4c50ed2
Jorge <jorge.softdevelop@gmail.com>
2018-05-10 15:46:16 -0400

    MOD: ajustes de validacion de codigo en el Sonar

--- soaint-sgd-enterprise-services/correspondencia-business-services/src/main/java/co/com/soaint/correspondencia/integration/service/rest/ConstraintViolationExceptionMapper.java
+++ soaint-sgd-enterprise-services/correspondencia-business-services/src/main/java/co/com/soaint/correspondencia/integration/service/rest/ConstraintViolationExceptionMapper.java
@@ -8,6 +8,7 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import co.com.soaint.correspondencia.domain.entity.ValidationError;
 
 @Provider
@@ -23,6 +24,7 @@ public class ConstraintViolationExceptionMapper implements ExceptionMapper<Const
         return Response.status(Response.Status.BAD_REQUEST).entity(errors)
                 .type(MediaType.APPLICATION_JSON).build();
     }
+
     private ValidationError toValidationError(ConstraintViolation constraintViolation) {
 
         String path = constraintViolation.getPropertyPath().toString();
@@ -34,16 +36,16 @@ public class ConstraintViolationExceptionMapper implements ExceptionMapper<Const
                 .field(path.split("\\.", -1)[2])
                 .clase(clase)
                 .metodo(path.split("\\.", -1)[0])
-                .preparedMessage(this.prepareMessage(clase,path,paquete,value))
+                .preparedMessage(this.prepareMessage(clase, path, paquete, value))
                 .paquete(paquete)
                 .value(value)
                 .build();
     }
 
-    private String prepareMessage(String clase,String path, String paquete, String value){
-        if (path.equals(null)) return "No validation error set.";
-        else if (clase.equals(null))clase="No class error set.";
+    private String prepareMessage(String clase, String path, String paquete, String value) {
+        if (path == null) return "No validation error set.";
+        else if (clase.equals(null)) clase = "No class error set.";
         String[] parsePath = path.split("\\.", -1);
-        return paquete+": "+clase+": "+"Error obteniendo el "+parsePath[2]+" en el metodo "+parsePath[0]+" con valor "+value;
+        return paquete + ": " + clase + ": " + "Error obteniendo el " + parsePath[2] + " en el metodo " + parsePath[0] + " con valor " + value;
     }
 }
