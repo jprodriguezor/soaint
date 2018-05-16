db30b240661f4753b4f99219aa8b188c042f8968
dagmarfr <dfrod2015@gmail.com>
2018-05-16 00:26:08 -0400

    fix: Arreglando problema de url con la constante motivo

--- soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/MotivoNoCreacionUdGatewayApi.java
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/MotivoNoCreacionUdGatewayApi.java
@@ -16,7 +16,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/motivo-no-creacon-ud-gateway-api")
+@Path("/motivo-no-creacion-ud-gateway-api")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Log4j2
