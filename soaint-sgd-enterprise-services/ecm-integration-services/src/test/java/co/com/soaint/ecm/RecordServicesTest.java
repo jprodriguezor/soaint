f97f526c5168b5d63c17628adeeddd89bf337bfc
Dasiel Otero Dartayet <dotero@gmail.com>
2018-05-17 17:05:01 -0400

    MOD ECM Tests

--- soaint-sgd-enterprise-services/ecm-integration-services/src/test/java/co/com/soaint/ecm/RecordServicesTest.java
+++ soaint-sgd-enterprise-services/ecm-integration-services/src/test/java/co/com/soaint/ecm/RecordServicesTest.java
@@ -1,12 +1,20 @@
 package co.com.soaint.ecm;
 
+import co.com.soaint.ecm.business.boundary.documentmanager.ContentControlAlfresco;
 import co.com.soaint.ecm.business.boundary.documentmanager.interfaces.ContentControl;
 import co.com.soaint.ecm.business.boundary.documentmanager.interfaces.IRecordServices;
 import co.com.soaint.ecm.business.boundary.documentmanager.interfaces.impl.RecordServices;
+import co.com.soaint.ecm.domain.entity.Conexion;
 import co.com.soaint.ecm.util.SystemParameters;
+import co.com.soaint.foundation.canonical.ecm.EntradaRecordDTO;
+import co.com.soaint.foundation.canonical.ecm.MensajeRespuesta;
 import co.com.soaint.foundation.canonical.ecm.UnidadDocumentalDTO;
 import co.com.soaint.foundation.framework.exceptions.SystemException;
 import org.apache.chemistry.opencmis.client.api.Folder;
+import org.apache.chemistry.opencmis.client.api.SessionFactory;
+import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl;
+import org.apache.chemistry.opencmis.commons.SessionParameter;
+import org.apache.chemistry.opencmis.commons.enums.BindingType;
 import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
@@ -21,9 +29,7 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import static co.com.soaint.ecm.business.boundary.documentmanager.interfaces.IRecordServices.RMA_IS_CLOSED;
 import static org.junit.Assert.*;
@@ -34,7 +40,9 @@ public class RecordServicesTest {
     @Autowired
     private IRecordServices recordServices;
     @Autowired
-    private ContentControl contentControl;
+    private ContentControlAlfresco contentControlAlfresco;
+
+    private Conexion conexion;
     String idSubCategoria = "";
     private String idPadre = "";
     @Value("${protocolo}")
@@ -94,15 +102,68 @@ public class RecordServicesTest {
     private Map<String, Object> disposicion = new HashMap<>();
 
     UnidadDocumentalDTO unidadDocumentalDTO =new UnidadDocumentalDTO();
-
+    private String urLecm = System.getProperty("URLecm");
     @Before
     public void setUp() throws Exception {
+
+        conexion = new Conexion();
+        //
+        //crear conexion
+
+
+        //Inicializar coneccion
+        Map<String, String> parameter = new HashMap<>();
+        // Credenciales del usuario
+        parameter.put(SessionParameter.USER, "admin");
+        parameter.put(SessionParameter.PASSWORD, "admin");
+
+        // configuracion de conexion
+        parameter.put(SessionParameter.ATOMPUB_URL, urLecm);
+        parameter.put(SessionParameter.ATOMPUB_URL, "http://192.168.3.245:8080/alfresco/api/-default-/public/cmis/versions/1.1/atom");
+        parameter.put(SessionParameter.BINDING_TYPE, BindingType.ATOMPUB.value());
+        parameter.put(SessionParameter.REPOSITORY_ID, "-default-");
+
+        // Object factory de Alfresco
+        parameter.put(SessionParameter.OBJECT_FACTORY_CLASS, "org.alfresco.cmis.client.impl.AlfrescoObjectFactoryImpl");
+
+        // Crear Sesion
+        SessionFactory factory = SessionFactoryImpl.newInstance();
+        conexion.setSession(factory.getRepositories(parameter).get(0).createSession());
+
+
+//Se llenan los datos de la unidad documental
+        unidadDocumentalDTO = new UnidadDocumentalDTO();
+        unidadDocumentalDTO.setInactivo(true);
+        //Calendar calendar
+        Calendar gregorianCalendar = GregorianCalendar.getInstance();
+        unidadDocumentalDTO.setFechaCierre(gregorianCalendar);
+        unidadDocumentalDTO.setId("1118");
+        unidadDocumentalDTO.setFechaExtremaInicial(gregorianCalendar);
+        unidadDocumentalDTO.setSoporte("electronico");
+        unidadDocumentalDTO.setNombreUnidadDocumental("UnidadDocumentalTest");
+        unidadDocumentalDTO.setFechaExtremaFinal(gregorianCalendar);
+        unidadDocumentalDTO.setCerrada(true);
+        unidadDocumentalDTO.setCodigoSubSerie("02312");
+        unidadDocumentalDTO.setCodigoSerie("0231");
+        unidadDocumentalDTO.setCodigoDependencia("10001040");
+        unidadDocumentalDTO.setDescriptor1("3434");
+        unidadDocumentalDTO.setDescriptor2("454545");
+        unidadDocumentalDTO.setAccion("ABRIR");
+        unidadDocumentalDTO.setInactivo(false);
+        unidadDocumentalDTO.setCerrada(false);
+        unidadDocumentalDTO.setEstado("Abierto");
+        unidadDocumentalDTO.setDisposicion("Eliminar");
+
+        MensajeRespuesta mensajeRespuesta = contentControlAlfresco.
+                crearUnidadDocumental(unidadDocumentalDTO, conexion.getSession());
+
+         unidadDocumentalDTO = (UnidadDocumentalDTO) mensajeRespuesta.getResponse().get("unidadDocumental");
 
     }
 
     @After
     public void tearDown() throws Exception {
-
+        contentControlAlfresco.eliminarUnidadDocumental(unidadDocumentalDTO.getId(), conexion.getSession());
         //Eliminar Record Folder
             WebTarget wt = ClientBuilder.newClient().target(SystemParameters.getParameter(SystemParameters.BUSINESS_PLATFORM_RECORD));
             wt.path("/record-folders/" + unidadDocumentalDTO.getId())
@@ -118,7 +179,18 @@ public class RecordServicesTest {
 
     @Test
     public void crearCarpetaRecord() {
+        EntradaRecordDTO entradaRecordDTO = new EntradaRecordDTO();
+        try {
+            entradaRecordDTO.setDependencia("1000.1040_GERENCIA NACIONAL DE GESTION DOCUMENTAL");
+            entradaRecordDTO.setSede("1000_VICEPRESIDENCIA");
+            entradaRecordDTO.setNombreCarpeta("RecordFolderTest");
+            entradaRecordDTO.setSerie("0231");
+            entradaRecordDTO.setSubSerie("02312");
 
+            recordServices.crearCarpetaRecord(entradaRecordDTO);
+        } catch (SystemException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
