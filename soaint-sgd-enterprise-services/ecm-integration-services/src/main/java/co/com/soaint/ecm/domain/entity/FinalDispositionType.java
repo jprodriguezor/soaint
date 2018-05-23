8186edf3d945e4dcc15f7bb014931d76d7276787
Jose L <josevazquez101987@gmail.com>
2018-05-23 01:03:00 -0400

    MOD: Tam Original Estampilla y Agregar Extension PDF

--- soaint-sgd-enterprise-services/ecm-integration-services/src/main/java/co/com/soaint/ecm/domain/entity/FinalDispositionType.java
+++ soaint-sgd-enterprise-services/ecm-integration-services/src/main/java/co/com/soaint/ecm/domain/entity/FinalDispositionType.java
@@ -2,6 +2,7 @@ package co.com.soaint.ecm.domain.entity;
 
 import lombok.AccessLevel;
 import lombok.Getter;
+import org.springframework.util.StringUtils;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -18,107 +19,35 @@ public enum FinalDispositionType implements Serializable {
 
     private static final Long serialVersionUID = 1L;
 
-    private final List<String> allTotalConservation = new ArrayList<>();
-    private final List<String> allEliminar = new ArrayList<>();
-    private final List<String> allSeleccionar = new ArrayList<>();
-    private final List<String> allMicrofilmar = new ArrayList<>();
-    private final List<String> allDigitalizar = new ArrayList<>();
+    private final List<String> dispositionList;
 
     FinalDispositionType(String key, String s0, String s1) {
-        switch (key) {
-            case "CT":
-                allTotalConservation.add(allTotalConservation.size(), key);
-                allTotalConservation.add(allTotalConservation.size(), key.toLowerCase());
-                allTotalConservation.add(allTotalConservation.size(), s0);
-                allTotalConservation.add(allTotalConservation.size(), s0.toUpperCase());
-                allTotalConservation.add(allTotalConservation.size(), s0.toLowerCase());
-                allTotalConservation.add(allTotalConservation.size(), s1);
-                allTotalConservation.add(allTotalConservation.size(), s1.toUpperCase());
-                allTotalConservation.add(allTotalConservation.size(), s1.toLowerCase());
-                break;
-            case "E":
-                allEliminar.add(allEliminar.size(), key);
-                allEliminar.add(allEliminar.size(), key.toLowerCase());
-                allEliminar.add(allEliminar.size(), s0);
-                allEliminar.add(allEliminar.size(), s0.toUpperCase());
-                allEliminar.add(allEliminar.size(), s0.toLowerCase());
-                break;
-            case "S":
-                allSeleccionar.add(allSeleccionar.size(), key);
-                allSeleccionar.add(allSeleccionar.size(), key.toLowerCase());
-                allSeleccionar.add(allSeleccionar.size(), s0);
-                allSeleccionar.add(allSeleccionar.size(), s0.toUpperCase());
-                allSeleccionar.add(allSeleccionar.size(), s0.toLowerCase());
-                break;
-            case "M":
-                allMicrofilmar.add(allMicrofilmar.size(), key);
-                allMicrofilmar.add(allMicrofilmar.size(), key.toLowerCase());
-                allMicrofilmar.add(allMicrofilmar.size(), s0);
-                allMicrofilmar.add(allMicrofilmar.size(), s0.toUpperCase());
-                allMicrofilmar.add(allMicrofilmar.size(), s0.toLowerCase());
-                break;
-            case "D":
-                allDigitalizar.add(allDigitalizar.size(), key);
-                allDigitalizar.add(allDigitalizar.size(), key.toLowerCase());
-                allDigitalizar.add(allDigitalizar.size(), s0);
-                allDigitalizar.add(allDigitalizar.size(), s0.toUpperCase());
-                allDigitalizar.add(allDigitalizar.size(), s0.toLowerCase());
-                break;
+        dispositionList = new ArrayList<>();
+        dispositionList.add(dispositionList.size(), key);
+        dispositionList.add(dispositionList.size(), key.toLowerCase());
+        dispositionList.add(dispositionList.size(), s0);
+        dispositionList.add(dispositionList.size(), s0.toUpperCase());
+        dispositionList.add(dispositionList.size(), s0.toLowerCase());
+        if (!StringUtils.isEmpty(s1)) {
+            dispositionList.add(dispositionList.size(), s1);
+            dispositionList.add(dispositionList.size(), s1.toUpperCase());
+            dispositionList.add(dispositionList.size(), s1.toLowerCase());
         }
     }
 
     public String getKey() {
-        switch (this) {
-            case SELECCIONAR:
-                return allSeleccionar.get(0);
-            case MICROFILMAR:
-                return allMicrofilmar.get(0);
-            case DIGITALIZAR:
-                return allDigitalizar.get(0);
-            case ELIMINAR:
-                return allEliminar.get(0);
-            default:
-                return allTotalConservation.get(0);
-        }
+        return dispositionList.get(0);
     }
 
     public boolean containsDisposition(String disposition) {
-        switch (this) {
-            case SELECCIONAR:
-                return allSeleccionar.contains(disposition);
-            case MICROFILMAR:
-                return allMicrofilmar.contains(disposition);
-            case DIGITALIZAR:
-                return allDigitalizar.contains(disposition);
-            case ELIMINAR:
-                return allEliminar.contains(disposition);
-            default:
-                return allTotalConservation.contains(disposition);
-        }
+        return dispositionList.contains(disposition);
     }
 
     public String getInDispositions() {
-        switch (this) {
-            case SELECCIONAR:
-                return getInDispositions(getAllSeleccionar());
-            case MICROFILMAR:
-                return getInDispositions(getAllMicrofilmar());
-            case DIGITALIZAR:
-                return getInDispositions(getAllDigitalizar());
-            case ELIMINAR:
-                return getInDispositions(getAllEliminar());
-            case CONSERVACION_TOTAL:
-                return getInDispositions(getAllTotalConservation());
-            default:
-                return "''";
-        }
-    }
-
-    private String getInDispositions(List<String> list) {
-        final int length = list.size();
+        final int length = dispositionList.size();
         final StringBuilder builder = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            final String disposition = list.get(i);
+            final String disposition = dispositionList.get(i);
             final String comma = (i == length - 1) ? "" : ",";
             builder.append("'").append(disposition).append("'").append(comma);
         }
