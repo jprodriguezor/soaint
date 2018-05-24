f659fdf11126d0b617320d17ebded7b3373a68cd
Marian <marivelba88@gmail.com>
2018-05-24 13:15:51 -0400

    creando la clase ProcessServiceTest

---
+++ soaint-sgd-enterprise-services/bpm-integration-services/src/main/java/co/com/soaint/bpm/services/integration/services/impl/ProcessServiceTest.java
@@ -0,0 +1,45 @@
+package co.com.soaint.bpm.services.integration.services.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProcessServiceTest {
+
+    @Test
+    public void listarProcesos() {
+    }
+
+    @Test
+    public void listarVariablesProcesos() {
+    }
+
+    @Test
+    public void listarProcesosInstanciaPorUsuarios() {
+    }
+
+    @Test
+    public void abortarProceso() {
+    }
+
+    @Test
+    public void iniciarProceso() {
+        
+    }
+
+    @Test
+    public void iniciarProcesoPorTercero() {
+    }
+
+    @Test
+    public void iniciarProcesoManual() {
+    }
+
+    @Test
+    public void enviarSenalProceso() {
+    }
+
+    @Test
+    public void senalInicioAutomatico() {
+    }
+}
