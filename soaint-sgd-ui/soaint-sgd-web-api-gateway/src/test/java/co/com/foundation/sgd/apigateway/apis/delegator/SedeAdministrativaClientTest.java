d6589ce9901013d11475928e96eac6696877270c
Jose <jacob1182@gmail.com>
2018-05-27 17:23:59 -0400

    apigateway: SedeAdministrativaClientTest - implementacion, se extrae (Client client) como dependencia de la clase SedeAdministrativaClient para realizar la prueba

---
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/test/java/co/com/foundation/sgd/apigateway/apis/delegator/SedeAdministrativaClientTest.java
@@ -0,0 +1,56 @@
+package co.com.foundation.sgd.apigateway.apis.delegator;
+
+import co.com.foundation.sgd.apigateway.apis.mocks.JaxRsUtils;
+import co.com.foundation.sgd.apigateway.rules.EnvironmentRule;
+import co.com.foundation.sgd.utils.SystemParameters;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class SedeAdministrativaClientTest {
+
+    @Rule
+    public EnvironmentRule environmentRule = new EnvironmentRule();
+
+    private String API_ENDPOINT = SystemParameters.getParameter(SystemParameters.BACKAPI_ENDPOINT_URL);
+
+    private SedeAdministrativaClient sedeAdministrativaClient;
+
+    private WebTarget wt;
+
+    private Client client;
+
+    @Before
+    public void setup() {
+
+        sedeAdministrativaClient = new SedeAdministrativaClient();
+
+        client = mock(Client.class);
+        wt = mock(WebTarget.class);
+
+        when(client.target(anyString())).thenReturn(wt);
+
+        ReflectionTestUtils.setField(sedeAdministrativaClient, "client", client);
+    }
+
+    @Test
+    public void list() {
+        // given
+        String path = "/organigrama-web-api/organigrama/sedes";
+        JaxRsUtils.mockGetPath(wt, path);
+
+        // when
+        sedeAdministrativaClient.list();
+
+        // then
+        verify(client).target(API_ENDPOINT);
+        verify(wt).path(path);
+    }
+}
