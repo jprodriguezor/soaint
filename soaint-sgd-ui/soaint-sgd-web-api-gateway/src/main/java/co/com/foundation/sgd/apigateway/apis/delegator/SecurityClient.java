659a6262c5e8f04a21fb017058fc401c0b97cb58
Jose <jacob1182@gmail.com>
2018-05-27 17:19:28 -0400

    apigateway: SecurityClientTest - implementacion, se extrae (Client client) como dependencia de la clase SecurityClient para realizar la prueba

--- soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/delegator/SecurityClient.java
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/main/java/co/com/foundation/sgd/apigateway/apis/delegator/SecurityClient.java
@@ -3,10 +3,9 @@ package co.com.foundation.sgd.apigateway.apis.delegator;
 import co.com.foundation.sgd.infrastructure.ApiDelegator;
 import co.com.foundation.sgd.utils.SystemParameters;
 import co.com.soaint.foundation.canonical.correspondencia.CredencialesDTO;
-import co.com.soaint.foundation.canonical.correspondencia.FuncionarioDTO;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
 
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
@@ -18,13 +17,15 @@ public class SecurityClient {
 
     private String endpoint = SystemParameters.getParameter(SystemParameters.BACKAPI_FUNCIONARIO_SERVICE_ENDPOINT_URL);
 
+    private Client client = ClientBuilder.newClient();
+
     public SecurityClient() {
         super();
     }
 
     public Response verificarCredenciales(CredencialesDTO credenciales) {
         log.info("Funcionario - [trafic] - obtener Funcionario with endpoint: " + endpoint);
-        WebTarget wt = ClientBuilder.newClient().target(endpoint);
+        WebTarget wt = client.target(endpoint);
         return wt.path("/funcionarios-web-api/funcionarios/verificar-credenciales")
                 .request()
                 .post(Entity.json(credenciales));
