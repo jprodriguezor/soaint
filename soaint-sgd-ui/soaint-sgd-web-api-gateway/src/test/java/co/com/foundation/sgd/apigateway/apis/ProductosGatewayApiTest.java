7a0f16c832d89528f8e537ba66a81968b591a2e4
Jose <jacob1182@gmail.com>
2018-05-29 21:53:25 -0400

    apigateway: ProductosGatewayApiTest - implementacion

---
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/test/java/co/com/foundation/sgd/apigateway/apis/ProductosGatewayApiTest.java
@@ -0,0 +1,45 @@
+package co.com.foundation.sgd.apigateway.apis;
+
+import co.com.foundation.sgd.apigateway.apis.delegator.ProductosClient;
+import co.com.foundation.test.mocks.ApiUtils;
+import co.com.foundation.test.mocks.JaxRsUtils;
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ProductosGatewayApiTest {
+
+    @Test
+    public void list() {
+
+        // given
+        ProductosGatewayApi gatewayApi = new ProductosGatewayApi();
+        String CONTENT = "{\"data\":100}";
+
+        Response theResponse = JaxRsUtils.mockResponse(String.class, CONTENT, Response.Status.OK);
+
+        ProductosClient productosClient = mock(ProductosClient.class);
+        when(productosClient.list()).thenReturn(theResponse);
+
+        ReflectionTestUtils.setField(gatewayApi, "productosClient", productosClient);
+
+        // when
+        Response response = gatewayApi.list();
+
+        // then
+        ApiUtils.assertThat(ProductosGatewayApi.class, "list")
+                .hasGetMapping("/productos-gateway-api")
+                .produces(MediaType.APPLICATION_JSON)
+                .consumes(MediaType.APPLICATION_JSON)
+                .hasJWTSecurity();
+
+        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        assertThat(response.getEntity()).isEqualTo(CONTENT);
+    }
+}
