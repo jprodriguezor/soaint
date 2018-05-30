a2f2388bc9ac24d796380375a1dcdc8843ebe6f9
Jose <jacob1182@gmail.com>
2018-05-29 22:34:55 -0400

    apigateway: TratamientoCortesiaGatewayApiTest - implementacion

---
+++ soaint-sgd-ui/soaint-sgd-web-api-gateway/src/test/java/co/com/foundation/sgd/apigateway/apis/TratamientoCortesiaGatewayApiTest.java
@@ -0,0 +1,44 @@
+package co.com.foundation.sgd.apigateway.apis;
+
+import co.com.foundation.sgd.apigateway.apis.delegator.TratamientoCortesiaClient;
+import co.com.foundation.test.mocks.ApiUtils;
+import co.com.foundation.test.mocks.JaxRsUtils;
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TratamientoCortesiaGatewayApiTest {
+
+    @Test
+    public void list() {
+        // given
+        TratamientoCortesiaGatewayApi gatewayApi = new TratamientoCortesiaGatewayApi();
+        String CONTENT = "{\"data\":100}";
+
+        Response theResponse = JaxRsUtils.mockResponse(String.class, CONTENT, Response.Status.OK);
+
+        TratamientoCortesiaClient tratamientoCortesiaClient = mock(TratamientoCortesiaClient.class);
+        when(tratamientoCortesiaClient.list()).thenReturn(theResponse);
+
+        ReflectionTestUtils.setField(gatewayApi, "tratamientoCortesiaClient", tratamientoCortesiaClient);
+
+        // when
+        Response response = gatewayApi.list();
+
+        // then
+        ApiUtils.assertThat(TratamientoCortesiaGatewayApi.class, "list")
+                .hasGetMapping("/tratamiento-cortesia-gateway-api")
+                .produces(MediaType.APPLICATION_JSON)
+                .consumes(MediaType.APPLICATION_JSON)
+                .hasJWTSecurity();
+
+        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        assertThat(response.getEntity()).isEqualTo(CONTENT);
+    }
+}
