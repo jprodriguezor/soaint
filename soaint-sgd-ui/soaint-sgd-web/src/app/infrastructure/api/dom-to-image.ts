05c3d7d4e827b3f5f84996fd331fc922d3c5383e
dagmarfr <dfrod2015@gmail.com>
2018-06-06 10:51:09 -0400

    fix: Agregando soporte para safari

--- soaint-sgd-ui/soaint-sgd-web/src/app/infrastructure/api/dom-to-image.ts
+++ soaint-sgd-ui/soaint-sgd-web/src/app/infrastructure/api/dom-to-image.ts
@@ -33,16 +33,38 @@ export interface  IDomToImage {
       return new Blob([arr],{type:'image/png'});
     });
   }
-
 }
+
+  class SafariDomToImage implements  IDomToImage{
+    convertToBlob(node): Promise<Blob> {
+      return html2canvas(node).then(canvas => {
+
+        const dataUrl  = canvas.toDataURL( );
+
+        const binStr = atob(dataUrl.split(',')[1]);
+
+        const  len = binStr.length;
+        const  arr = new Uint8Array(len);
+
+        for (let i = 0; i < len; i++ ) {
+          arr[i] = binStr.charCodeAt(i);
+        }
+
+        return new Blob([arr],{type:'image/png'});
+      });
+    }
+  }
 
 export  class DomToImageFactory {
 
   readonly browser = detectBrowser.detect();
   getInstance():IDomToImage{
+    
+    switch(this.browser.name){
 
-    if(this.browser.name == 'edge')
-      return new EdgeDomToImage();
+      case 'edge' :  return new EdgeDomToImage();
+      case 'safari' : return new SafariDomToImage();
+    }
 
     return new DomToImage();
   }
