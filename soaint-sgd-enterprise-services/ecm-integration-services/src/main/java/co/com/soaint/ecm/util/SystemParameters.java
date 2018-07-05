06a3ffec3fedd163aee9d66bba0401785f38cf25
Jose <josevazquez101987@gmail.com>
2018-07-05 17:03:04 -0400

    Mod: System getProperty

--- soaint-sgd-enterprise-services/ecm-integration-services/src/main/java/co/com/soaint/ecm/util/SystemParameters.java
+++ soaint-sgd-enterprise-services/ecm-integration-services/src/main/java/co/com/soaint/ecm/util/SystemParameters.java
@@ -25,29 +25,7 @@ public final class SystemParameters {
      * @return
      */
     public static String getParameter(final String parameterName) {
-        switch (parameterName) {
-            case BUSINESS_PLATFORM_ENDPOINT_ECM:
-                return "http://192.168.3.245:8080/alfresco/api/-default-/public/cmis/versions/1.1/atom";
-            case BUSINESS_PLATFORM_USER:
-                return "admin";
-            case BUSINESS_PLATFORM_PASS:
-                return "admin";
-            case BUSINESS_PLATFORM_RECORD:
-                return "http://192.168.3.245:8080/alfresco/api/-default-/public/gs/versions/1";
-            case API_SEARCH_ALFRESCO:
-                return "http://192.168.3.245:8080/alfresco/api/-default-/public/search/versions/1/search";
-            case API_CORE_ALFRESCO:
-                return "http://192.168.3.245:8080/alfresco/api/-default-/public/alfresco/versions/1";
-            case API_SERVICE_ALFRESCO:
-                return "http://192.168.3.245:8080/alfresco/service/api/node/workspace/SpacesStore/";
-            case BUSINESS_PLATFORM_ENDPOINT_BPM:
-                return "http://192.168.3.242:28080/bpm-integration-services/apis";
-            case BUSINESS_PLATFORM_ENDPOINT_CORRESPONDENCIA:
-                return "http://192.168.3.242:28080/correspondencia-business-services/services";
-            default:
-                return "";
-        }
-        //return System.getProperty(parameterName);
+        return System.getProperty(parameterName);
     }
 
 }
